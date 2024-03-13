d77a2e000ecb031fe814ff13e9b52c64fb829e5e
1hyung <161712242+1hyung@users.noreply.github.com>
2024-03-14 00:34:11 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.kt
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.kt
@@ -1,6 +1,9 @@
 class Solution {
     fun solution(num1: Int, num2: Int): Int {
-        var answer = num1 / num2
-        return answer
+        var answer: Int = 0
+        if (0 < num1 && num2 <= 100){
+            
+        }
+        return num1 / num2
     }
 }
