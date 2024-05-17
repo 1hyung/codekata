3f7b60416fcd9b023edb05d6edc6c5f498c63f53
1hyung <161712242+1hyung@users.noreply.github.com>
2024-05-17 20:39:05 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.04 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
@@ -1,29 +1,24 @@
-import java.util.*
-
 class Solution {
     fun solution(ingredient: IntArray): Int {
-        val st = Stack<Int>()
+        val st = mutableListOf<Int>()
     
         var answer: Int = 0
-        for(ing in ingredient){
-            st.push(ing)
+        for (ing in ingredient) {
+            st.add(ing)
             
-            if(st.size >= 4){
-                val peek1 = st.peek()
-                st.pop()
-                val peek2 = st.peek()
-                st.pop()
-                val peek3 = st.peek()
-                st.pop()
-                val peek4 = st.peek()
-                st.pop()
+            if (st.size >= 4) {
+                val peek1 = st.removeAt(st.size - 1)
+                val peek2 = st.removeAt(st.size - 1)
+                val peek3 = st.removeAt(st.size - 1)
+                val peek4 = st.removeAt(st.size - 1)
                 
-                if((peek4 == 1) && (peek3 == 2) && (peek2 == 3) && (peek1 == 1)) answer++;
-                else{
-                    st.push(peek4)
-                    st.push(peek3)
-                    st.push(peek2)
-                    st.push(peek1)
+                if ((peek4 == 1) && (peek3 == 2) && (peek2 == 3) && (peek1 == 1)) {
+                    answer++
+                } else {
+                    st.add(peek4)
+                    st.add(peek3)
+                    st.add(peek2)
+                    st.add(peek1)
                 }
             }
         }
