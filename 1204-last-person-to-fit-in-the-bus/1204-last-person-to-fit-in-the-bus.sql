ea175344ee28d856e31027994fea43f74c45452e
1hyung <161712242+1hyung@users.noreply.github.com>
2024-06-26 21:47:32 +0900

    Time: 754 ms (89.49%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,10 @@
+with temp_01 as(
+select turn, person_id, person_name, weight
+    ,sum(weight) over(order by turn) as total_weight
+from queue
+)
+select distinct last_value(person_name)
+		over(order by total_weight
+        	rows between unbounded preceding and unbounded following) as person_name
+from temp_01
+where total_weight <= 1000
