8c04cece0f85866c07b30a406ee83454ce0e1fc5
1hyung <161712242+1hyung@users.noreply.github.com>
2024-07-20 00:59:48 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -4,6 +4,7 @@ class Solution {
             angle in 1 until 90 -> 1 // 예각 (0 < angle < 90)
             angle == 90 -> 2 // 직각 (angle = 90)
             angle in 91 until 180 -> 3 // 둔각 (90 < angle < 180)
+            angle == 180 -> 4 // 평각 (angle = 180)
             else -> 4
         }
     }
