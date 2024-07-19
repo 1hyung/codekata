1ad8671c1e04258415f4d9002a59340cfa9ef50b
1hyung <161712242+1hyung@users.noreply.github.com>
2024-07-20 00:57:34 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -1,13 +1,10 @@
 class Solution {
     fun solution(angle: Int): Int {
-        if (0 < angle && angle < 90) {
-            return 1
-        } else if (angle === 90) {
-            return 2 
-        } else if (90 < angle && angle < 180) {
-            return 3
-        } else {
-            return 4
+        return when{
+            angle in 1 until 90 -> 1 // 예각 (0 < angle < 90)
+            angle == 90 -> 2 // 직각 (angle = 90)
+            angle in 91 until 180 -> 3 // 둔각 (90 < angle < 180)
+            else -> 4
         }
     }
 }
