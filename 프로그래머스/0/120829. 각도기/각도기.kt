71878a4b1f30d203776a2fcfc525ed7d6b1eef33
1hyung <161712242+1hyung@users.noreply.github.com>
2024-07-20 01:01:45 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -5,7 +5,7 @@ class Solution {
             angle == 90 -> 2 // 직각 (angle = 90)
             angle in 91 until 180 -> 3 // 둔각 (90 < angle < 180)
             angle == 180 -> 4 // 평각 (angle = 180)
-            else -> 4
+            else -> throw IllegalArgumentException("Invalid angle") // 유효하지 않은 각도 처리
         }
     }
 }
