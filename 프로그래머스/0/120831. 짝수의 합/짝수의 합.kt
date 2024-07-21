a50add608cd517ae021b6ed25b6698052e4867b2
1hyung <161712242+1hyung@users.noreply.github.com>
2024-07-21 23:26:52 +0900

    [level 0] Title: 짝수의 합, Time: 11.04 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
@@ -1,12 +1,6 @@
 class Solution {
     fun solution(n: Int): Int {
-        var answer: Int = 0
-        
-        for (i: Int in 1..n) {
-            if (i%2==0){
-                answer += i
-            }
-        }
-        return answer
+        // 1부터 n까지의 숫자 중 짝수만 필터링하여 합을 구함
+        return (1..n).filter{it%2 == 0}.sum()
     }
 }
