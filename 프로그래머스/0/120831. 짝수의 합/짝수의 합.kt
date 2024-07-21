e4a86d5a39c9d0bd6e4269cd731f83fa06c3cd28
1hyung <161712242+1hyung@users.noreply.github.com>
2024-07-21 23:23:21 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
@@ -2,8 +2,8 @@ class Solution {
     fun solution(n: Int): Int {
         var answer: Int = 0
         
-        for(i:Int in 1..n){
-            if(i%2==0){
+        for (i: Int in 1..n) {
+            if (i%2==0){
                 answer += i
             }
         }
