a16c5d0d79151b6fa8346227ea2377da057e7bec
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-02 23:41:56 +0900

    [level 1] Title: 하샤드 수, Time: 8.78 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/README.md
+++ 프로그래머스/1/12947. 하샤드 수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 하샤드 수 - 12947 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12947?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12947) 
 
 ### 성능 요약
 
-메모리: 61.5 MB, 시간: 0.21 ms
+메모리: 60.3 MB, 시간: 8.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 22:36:28
+2024년 08월 02일 23:41:54
 
 ### 문제 설명
 
--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,11 +1,9 @@
 class Solution {
     fun solution(x: Int): Boolean {
-        var answer = true
-        var sum = 0
+        // x를 문자열로 변환한 뒤, 각 자릿수를 정수로 변환하여 합을 구합니다.
+        val sumOfDigits = x.toString().map { it.toString().toInt() }.sum()
         
-        x.toString().map {
-            sum += (it.toInt() - '0'.toInt())
-        }
-        return x % sum == 0
+        // x가 자릿수 합으로 나누어 떨어지는지 확인합니다.
+        return x % sumOfDigits == 0
     }
 }
