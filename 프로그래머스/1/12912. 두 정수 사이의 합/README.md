4122a8faf8a8a84c107017323dd357bf6fc2e718
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-03 23:51:11 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 11.23 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/README.md
+++ 프로그래머스/1/12912. 두 정수 사이의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 정수 사이의 합 - 12912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12912) 
 
 ### 성능 요약
 
-메모리: 62.4 MB, 시간: 0.07 ms
+메모리: 59 MB, 시간: 11.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 01일 08:10:41
+2024년 08월 03일 23:51:08
 
 ### 문제 설명
 
--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -1,17 +1,15 @@
 class Solution {
     fun solution(a: Int, b: Int): Long {
-        var sum: Long = 0
+        // a와 b 중 작은 값과 큰 값을 결정합니다.
+        val start = minOf(a, b)
+        val end = maxOf(a, b)
         
-        if (a <= b) {
-            for (i in a..b) {
-                sum += i
-            }
+        // 작은 값부터 큰 값까지의 합을 계산합니다.
+        var sum: Long = 0
+        for (i in start..end) {
+            sum += i
         }
-        else {
-            for (i in b..a) {
-                sum += i
-            }
-        }
-        return sum.toLong()
+        
+        return sum
     }
 }
