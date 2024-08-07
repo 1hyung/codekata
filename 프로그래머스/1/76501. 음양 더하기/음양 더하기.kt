6661e6cbf39e274ad2273e1be9a60c6644cde9de
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-07 23:10:55 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
@@ -1,12 +1,15 @@
 class Solution {
     fun solution(absolutes: IntArray, signs: BooleanArray): Int {
-        var sum = 0
-        signs.forEachIndexed { index, i ->
-            when (i) {
-                true -> sum += absolutes[index]
-                else -> sum -= absolutes[index]
+        var sum = 0 // 결과 합계를 저장할 변수
+
+        for (i in absolutes.indices) {
+            if (signs[i]) {
+                sum += absolutes[i] // signs[i]가 참이면 양수로 더함
+            } else {
+                sum -= absolutes[i] // signs[i]가 거짓이면 음수로 더함
             }
         }
-        return sum
+
+        return sum // 최종 합계 반환
     }
 }
