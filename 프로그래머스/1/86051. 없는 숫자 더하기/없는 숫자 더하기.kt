7a3a1a04962e41a86bca0bc85e926d3450dbdbf3
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-09 20:54:58 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 40.17 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -1,3 +1,12 @@
 class Solution {
-    fun solution(numbers: IntArray) = (0..9).filter { !numbers.contains(it) }.sum()
+    fun solution(numbers: IntArray): Int {
+        // 0부터 9까지의 모든 숫자를 포함하는 집합을 생성
+        val fullSet = (0..9).toSet()
+        
+        // 주어진 numbers 배열을 집합으로 변환
+        val numbersSet = numbers.toSet()
+        
+        // 0부터 9까지의 집합에서 numbers 집합을 뺀 후, 남은 숫자들의 합을 반환
+        return fullSet.subtract(numbersSet).sum()
+    }
 }
