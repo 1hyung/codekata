d6aa70997aed10985a8de26deb2c8ce4c7abac53
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-14 21:41:59 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 8.47 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,5 +1,27 @@
 class Solution {
     fun solution(left: Int, right: Int): Int {
-        return (left..right).map { i -> if ((1..i).filter { i % it == 0 }.size % 2 == 0) i else -i }.sum()
+        var result = 0
+
+        for (num in left..right) {
+            val divisorCount = countDivisors(num)
+            if (divisorCount % 2 == 0) {
+                result += num
+            } else {
+                result -= num
+            }
+        }
+
+        return result
+    }
+
+    // 주어진 숫자의 약수 개수를 구하는 함수
+    private fun countDivisors(num: Int): Int {
+        var count = 0
+        for (i in 1..num) {
+            if (num % i == 0) {
+                count++
+            }
+        }
+        return count
     }
 }
