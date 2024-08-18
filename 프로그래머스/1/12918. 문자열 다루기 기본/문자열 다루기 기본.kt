093aa9fd65115fd45bf10cfa438e3eb31c76589d
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-18 21:30:13 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 60.7 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -1,3 +1,11 @@
 class Solution {
-    fun solution(s: String) = (s.length == 4 || s.length == 6) && s.all{ it.isDigit() }
+    fun solution(s: String): Boolean {
+        // 문자열의 길이가 4 또는 6인지 확인
+        if (s.length != 4 && s.length != 6) {
+            return false
+        }
+        
+        // 문자열이 숫자로만 구성되어 있는지 확인
+        return s.all { it.isDigit() }
+    }
 }
