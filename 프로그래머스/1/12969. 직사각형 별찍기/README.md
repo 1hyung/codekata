39155d18c5d40151cf2c075c4b9cb115f019acf8
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-20 23:04:20 +0900

    [level 1] Title: 직사각형 별찍기, Time: 265.87 ms, Memory: 47.2 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/README.md
+++ 프로그래머스/1/12969. 직사각형 별찍기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 직사각형 별찍기 - 12969 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12969) 
 
 ### 성능 요약
 
-메모리: 58.4 MB, 시간: 302.82 ms
+메모리: 47.2 MB, 시간: 265.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 22:41:15
+2024년 08월 20일 23:04:18
 
 ### 문제 설명
 
--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -1,10 +1,12 @@
 fun main(args: Array<String>) {
-    val (a, b) = readLine()!!.split(' ').map(String::toInt)
-
-    for (i in 0 until b) {
-        for (j in 0 until a) {
-            print("*")
-        }
-        println()
+    // 표준 입력으로 두 개의 정수 n과 m을 받습니다.
+    val (n, m) = readLine()!!.split(' ').map { it.toInt() }
+    
+    // 가로로 n개의 별을 가진 문자열을 만듭니다.
+    val row = "*".repeat(n)
+    
+    // 이 문자열을 m번 반복해서 출력합니다.
+    for (i in 1..m) {
+        println(row)
     }
 }
