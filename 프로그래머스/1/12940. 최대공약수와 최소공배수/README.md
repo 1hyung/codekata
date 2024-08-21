6fda7f607406770ec3e0635d48959a6096548ad3
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-21 23:08:54 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
 
 ### 성능 요약
 
-메모리: 61.6 MB, 시간: 0.01 ms
+메모리: 60.3 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 19일 01:26:07
+2024년 08월 21일 23:08:51
 
 ### 문제 설명
 
--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -1,9 +1,12 @@
 class Solution {
     fun solution(n: Int, m: Int): IntArray {
-        val gcd = gcd(n, m)
-
-        return intArrayOf(gcd, n * m / gcd)
+        val gcd = gcd(n, m)  // 최대공약수 계산
+        val lcm = n * m / gcd  // 최소공배수 계산
+        return intArrayOf(gcd, lcm)
     }
 
-    tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+    // 유클리드 호제법을 사용한 최대공약수 계산 함수
+    private fun gcd(a: Int, b: Int): Int {
+        return if (b == 0) a else gcd(b, a % b)
+    }
 }
