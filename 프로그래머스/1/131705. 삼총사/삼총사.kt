61b926fdf1888979aeb2faa80339b9d4364d97c2
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-24 21:14:51 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.kt
+++ 프로그래머스/1/131705. 삼총사/삼총사.kt
@@ -1,18 +1,17 @@
 class Solution {
-    fun solution(number: IntArray): Int {            
+    fun solution(number: IntArray): Int {
+        var count = 0
 
-        var answer: Int = 0
-
-        for(i in 0 until number.size) {
-            for(j in i+1 until number.size step 1) {
-                for(k in j+1 until number.size step 1) {
-                    if (number[i] + number[j] + number[k] === 0) {
-                        answer++;
+        for (i in 0 until number.size - 2) {
+            for (j in i + 1 until number.size - 1) {
+                for (k in j + 1 until number.size) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        count++
                     }
                 }
             }
         }
 
-        return answer
+        return count
     }
 }
