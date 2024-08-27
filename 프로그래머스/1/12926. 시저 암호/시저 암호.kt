b51edab9daba7bf2e84f6ab77ef4da616974cdbe
1hyung <161712242+1hyung@users.noreply.github.com>
2024-08-27 22:23:11 +0900

    [level 1] Title: 시저 암호, Time: 21.45 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.kt
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.kt
@@ -1,14 +1,10 @@
 class Solution {
     fun solution(s: String, n: Int): String {
-        return s.toCharArray().map{
-            when(it){
-                in 'A'..'Z' -> {
-                    if(it + n > 'Z') it + n - 26 else it + n
-                }in 'a'..'z' -> {
-                    if(it + n > 'z') it + n - 26 else it + n
-                }else -> {
-                    it
-                }
+        return s.map { char ->
+            when {
+                char.isUpperCase() -> 'A' + (char - 'A' + n) % 26
+                char.isLowerCase() -> 'a' + (char - 'a' + n) % 26
+                else -> char
             }
         }.joinToString("")
     }
