10235a52a1d1b3498f7323218772f7a1123afbd6
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-06 20:13:00 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -1,17 +1,22 @@
 class Solution {
     fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
-        var answer = "Yes"
-        var idx1 = 0
-        var idx2 = 0
-        goal.forEach { 
-            if(idx1 < cards1.size && cards1[idx1] == it){
-                idx1++
-            } else if(idx2 < cards2.size && cards2[idx2] == it){
-                idx2++
-            } else {
-                return "No"
+        var index1 = 0  // cards1의 포인터
+        var index2 = 0  // cards2의 포인터
+
+        for (word in goal) {
+            when {
+                index1 < cards1.size && cards1[index1] == word -> {
+                    index1++  // cards1에서 카드를 사용
+                }
+                index2 < cards2.size && cards2[index2] == word -> {
+                    index2++  // cards2에서 카드를 사용
+                }
+                else -> {
+                    return "No"  // goal의 단어가 cards1, cards2와 일치하지 않으면 불가능
+                }
             }
         }
-        return answer
+
+        return "Yes"  // 모든 단어를 순서대로 만들 수 있으면 "Yes"
     }
 }
