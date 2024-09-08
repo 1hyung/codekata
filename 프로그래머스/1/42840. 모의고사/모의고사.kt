728caddeebfb1fb2240f56de76c602c140ba7411
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-08 17:37:00 +0900

    [level 1] Title: 모의고사, Time: 30.66 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.kt
+++ 프로그래머스/1/42840. 모의고사/모의고사.kt
@@ -1,31 +1,27 @@
-import java.util.*
-
-internal class Solution {
+class Solution {
     fun solution(answers: IntArray): IntArray {
-        val score = IntArray(3)
-        val a = intArrayOf(1, 2, 3, 4, 5)
-        val b = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
-        val c = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+        // 수포자들의 찍기 패턴 정의
+        val pattern1 = intArrayOf(1, 2, 3, 4, 5)
+        val pattern2 = intArrayOf(2, 1, 2, 3, 2, 4, 2, 5)
+        val pattern3 = intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)
+
+        // 수포자들의 맞힌 문제 개수를 저장할 배열
+        val scores = intArrayOf(0, 0, 0)
+
+        // 정답과 패턴을 비교하여 점수 계산
         for (i in answers.indices) {
-            if (answers[i] == a[i % a.size]) score[0]++
-            if (answers[i] == b[i % b.size]) score[1]++
-            if (answers[i] == c[i % c.size]) score[2]++
+            if (answers[i] == pattern1[i % pattern1.size]) scores[0]++
+            if (answers[i] == pattern2[i % pattern2.size]) scores[1]++
+            if (answers[i] == pattern3[i % pattern3.size]) scores[2]++
         }
-        val max = score[0].coerceAtLeast(score[1].coerceAtLeast(score[2]))
-        val list: MutableList<Int> = ArrayList()
-        if (max == score[0]) {
-            list.add(1)
-        }
-        if (max == score[1]) {
-            list.add(2)
-        }
-        if (max == score[2]) {
-            list.add(3)
-        }
-        val answer = IntArray(list.size)
-        for (i in list.indices) {
-            answer[i] = list[i]
-        }
-        return answer
+
+        // 최고 점수를 찾음
+        val maxScore = scores.maxOrNull()
+
+        // 최고 점수를 받은 수포자(1, 2, 3)를 찾아서 반환
+        return scores.indices
+            .filter { scores[it] == maxScore }
+            .map { it + 1 }
+            .toIntArray()
     }
 }
