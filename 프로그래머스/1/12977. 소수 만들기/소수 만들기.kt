517a815b1708b177d7b5195e1595e9b6d1b529cb
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-09 22:22:55 +0900

    [level 1] Title: 소수 만들기, Time: 2.64 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -1,18 +1,29 @@
 class Solution {
     fun solution(nums: IntArray): Int {
-        
-        var answer = 0
+        var count = 0
+
+        // 3개의 숫자를 선택하여 합을 구하는 부분
         for (i in 0 until nums.size - 2) {
             for (j in i + 1 until nums.size - 1) {
                 for (k in j + 1 until nums.size) {
-                    if (isPrime(nums[i] + nums[j] + nums[k])) answer++
+                    val sum = nums[i] + nums[j] + nums[k]
+                    // 합이 소수인지 확인
+                    if (isPrime(sum)) {
+                        count++
+                    }
                 }
             }
         }
-        return answer
-        
+
+        return count
     }
-    
-    fun isPrime(n: Int): Boolean = n > 1 && (2 until n).none { n % it == 0 }
-    
+
+    // 소수 판별 함수
+    private fun isPrime(n: Int): Boolean {
+        if (n < 2) return false
+        for (i in 2..Math.sqrt(n.toDouble()).toInt()) {
+            if (n % i == 0) return false
+        }
+        return true
+    }
 }
