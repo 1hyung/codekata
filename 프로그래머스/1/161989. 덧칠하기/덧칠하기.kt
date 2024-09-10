b312764088105ed8991ab908a8c6328120dbb52f
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-10 17:13:41 +0900

    [level 1] Title: 덧칠하기, Time: 1.51 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun solution(n: Int, m: Int, section: IntArray): Int {
-        var curM = section[0]
-        var answer = 1
+        var count = 0
+        var currentPos = 0  // 현재 롤러가 칠한 마지막 구역
 
-        for (i in 0 until section.size) {
-            if (section[i] <= curM + m - 1) continue
-            else {
-
-                curM = section[i]
-
-                answer++
+        for (s in section) {
+            if (s > currentPos) {
+                // 현재 롤러가 칠할 수 없는 구역이 나오면 롤러로 그 구역부터 m만큼 칠함
+                currentPos = s + m - 1  // 롤러의 끝 구역 계산
+                count++  // 페인트칠 횟수 증가
             }
         }
-        return answer
+
+        return count
     }
 }
