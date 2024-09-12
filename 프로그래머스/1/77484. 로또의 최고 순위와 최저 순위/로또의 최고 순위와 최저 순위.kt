750e965bdfcb30f9a527bf3a1aa5e3ae73981398
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-12 21:12:52 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 20.25 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.kt
@@ -1,31 +1,25 @@
 class Solution {
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        
-        var zeros = 0 
-        var correct = 0 
-        for(n in lottos){
-            if(n == 0) zeros++
-            else if(win_nums.contains(n)) correct++
+        // 0의 개수와 맞춘 번호의 개수를 계산
+        val zeros = lottos.count { it == 0 }
+        val correct = lottos.count { it in win_nums }
+
+        // 맞춘 개수에 따른 순위를 계산하는 함수
+        fun getRank(matches: Int): Int {
+            return when (matches) {
+                6 -> 1
+                5 -> 2
+                4 -> 3
+                3 -> 4
+                2 -> 5
+                else -> 6
+            }
         }
-        
-            answer += when(zeros + correct){
-            6-> 1
-            5-> 2
-            4-> 3
-            3-> 4 
-            2-> 5
-            else -> 6
-        } 
- 
-        answer += when(correct){
-            6-> 1
-            5-> 2
-            4-> 3
-            3-> 4 
-            2-> 5
-            else -> 6
-        }
-        return answer
+
+        // 최고 순위는 0이 모두 맞는 경우, 최저 순위는 0이 모두 틀린 경우
+        val maxRank = getRank(correct + zeros)
+        val minRank = getRank(correct)
+
+        return intArrayOf(maxRank, minRank)
     }
 }
