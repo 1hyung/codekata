2206688a42a15fc2c42d6d712d67ed423d1b8b71
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-14 16:18:05 +0900

    [level 1] Title: 체육복, Time: 22.62 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.kt
+++ 프로그래머스/1/42862. 체육복/체육복.kt
@@ -2,6 +2,7 @@ class Solution {
     fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
         var answer = 0
 
+		// MutalbeList로 변환 후 서로 갖고있는 요소 삭제
         lost.sort()
         reserve.sort()
         var lostList = lost.toMutableList()
@@ -15,16 +16,17 @@ class Solution {
         lostList.removeAll(removed)
         reserveList.removeAll(removed)
 
+		// 순환 시작
         for(i in lostList.indices) {
             for(j in reserveList.indices) {
                 if(reserveList[j] == lostList[i] + 1 || reserveList[j] == lostList[i] - 1) {
-                    reserveList.removeAt(j) 
+                    reserveList.removeAt(j) // 이미 빌려준 사람은 리스트에서 삭제
                     ++ answer
                     break
                 }
             }
         }
 
-        return n - (lostList.size - answer) 
+        return n - (lostList.size - answer) // 전체 인원 - (빌리기 전 잃어버린 사람 수 - 빌린 사람)
     }
 }
