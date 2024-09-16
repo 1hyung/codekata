f6072844132538e4945c37600f6b0de914f762bf
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-17 07:28:15 +0900

    [level 1] Title: 둘만의 암호, Time: 12.83 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.kt
@@ -1,14 +1,16 @@
 class Solution {
     fun solution(s: String, skip: String, index: Int): String {
+        // 'a'부터 'z'까지 중 skip에 없는 알파벳만 리스트로 만듦
         val alphabet = ('a'..'z').filterNot { it in skip }
         val result = StringBuilder()
 
+        // s의 각 문자를 변환
         for (char in s) {
             val currentIndex = alphabet.indexOf(char)
             val newIndex = (currentIndex + index) % alphabet.size
             result.append(alphabet[newIndex])
         }
 
-        return result.toString()
+        return result.toString()  // 변환된 문자열 반환
     }
 }
