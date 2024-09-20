d43b8fc7d28b924dc47377b337bf3c1d2302bf44
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-21 03:54:16 +0900

    [level 1] Title: 달리기 경주, Time: 398.94 ms, Memory: 356 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.kt
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.kt
@@ -1,31 +1,30 @@
 class Solution {
     fun solution(players: Array<String>, callings: Array<String>): Array<String> {
-        val answer = Array(players.size) { "" }
-        val rankMap = mutableMapOf<Int, String>() 
-        val playerMap = mutableMapOf<String, Int>() 
-
+        // 선수 이름을 키로, 현재 등수를 값으로 저장하는 맵을 생성
+        val positionMap = mutableMapOf<String, Int>()
         for (i in players.indices) {
-            rankMap[i] = players[i]
-            playerMap[players[i]] = i
+            positionMap[players[i]] = i // 선수의 현재 등수를 저장
         }
+        
+        // 추월할 때마다 순서를 갱신
+        for (name in callings) {
+            val currentPosition = positionMap[name]!! // 추월할 선수의 현재 위치
+            
+            if (currentPosition > 0) { // 맨 앞 선수는 추월하지 않음
+                // 앞에 있는 선수와 자리 바꾸기
+                val previousPosition = currentPosition - 1
+                val previousPlayer = players[previousPosition]
 
-        callings.forEach { calledPlayer ->
-            val calledPlayerRank = playerMap[calledPlayer]!! - 1
+                // 선수 배열에서 자리 변경
+                players[previousPosition] = name
+                players[currentPosition] = previousPlayer
 
-            val overtakenPlayer = rankMap[calledPlayerRank]!!
-            val overtakenPlayerRank = calledPlayerRank + 1
-
-            playerMap[calledPlayer] = calledPlayerRank
-            rankMap[calledPlayerRank] = calledPlayer
-
-            playerMap[overtakenPlayer] = overtakenPlayerRank
-            rankMap[overtakenPlayerRank] = overtakenPlayer
+                // 맵에서도 선수들의 등수 갱신
+                positionMap[name] = previousPosition
+                positionMap[previousPlayer] = currentPosition
+            }
         }
-
-        rankMap.forEach { (rank, player) ->
-            answer[rank] = player
-        }
-
-        return answer
+        
+        return players // 경주가 끝난 후 최종 선수 배열 반환
     }
 }
