05faae23eefb50ae0d4f2616d28bdc2684e9e07c
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-22 00:59:33 +0900

    [level 1] Title: 공원 산책, Time: 14.12 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.kt
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.kt
@@ -1,42 +1,54 @@
 class Solution {
     fun solution(park: Array<String>, routes: Array<String>): IntArray {
-        var answer = intArrayOf(0, 0)
-        val order = mapOf(
-            "N" to intArrayOf(-1, 0),
-            "S" to intArrayOf(1, 0),
-            "W" to intArrayOf(0, -1),
-            "E" to intArrayOf(0, 1)
+        // 공원의 크기를 변수로 설정
+        val height = park.size
+        val width = park[0].length
+
+        // 시작 위치(S)를 찾음
+        var currentPos = Pair(0, 0)
+        for (i in park.indices) {
+            if ('S' in park[i]) {
+                currentPos = Pair(i, park[i].indexOf('S'))
+                break
+            }
+        }
+
+        // 방향별 이동 좌표 변화를 저장하는 맵
+        val directionMap = mapOf(
+            "N" to Pair(-1, 0), // 북쪽
+            "S" to Pair(1, 0),  // 남쪽
+            "W" to Pair(0, -1), // 서쪽
+            "E" to Pair(0, 1)   // 동쪽
         )
 
-        loop@ for (i in park.indices) {
-            for (j in park[i].indices) {
-                if (park[i][j] == 'S') {
-                    answer = intArrayOf(i, j)
-                    break@loop
-                }
-            }
-        }
+        // 명령에 따라 이동을 처리
+        for (route in routes) {
+            val (direction, steps) = route.split(" ")
+            val move = directionMap[direction]!!
+            val stepCount = steps.toInt()
 
-        routes.forEach {
-            val parts = it.split(" ")
-            val move = order[parts[0]]!!
-            val count = parts[1].toInt()
-            var nr = answer[0]
-            var nc = answer[1]
+            var newPos = currentPos
+            var isBlocked = false
 
-            for (i in 1..count) {
-                nr += move[0]
-                nc += move[1]
-                if (nr !in park.indices || nc !in park[0].indices || park[nr][nc] == 'X') {
-                    nr = answer[0]
-                    nc = answer[1]
+            // 주어진 방향으로 한 칸씩 이동하면서 장애물이나 공원 밖인지 확인
+            for (step in 1..stepCount) {
+                val nextPos = Pair(newPos.first + move.first, newPos.second + move.second)
+                
+                // 공원 밖으로 나가는 경우 처리
+                if (nextPos.first !in 0 until height || nextPos.second !in 0 until width || park[nextPos.first][nextPos.second] == 'X') {
+                    isBlocked = true
                     break
                 }
+                newPos = nextPos
             }
-            answer[0] = nr
-            answer[1] = nc
+
+            // 이동이 가능하면 위치를 업데이트
+            if (!isBlocked) {
+                currentPos = newPos
+            }
         }
 
-        return answer
+        // 최종 위치 반환
+        return intArrayOf(currentPos.first, currentPos.second)
     }
 }
