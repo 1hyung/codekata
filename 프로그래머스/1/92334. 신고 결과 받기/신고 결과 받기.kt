ab0b69cf48425f0ad09fa3fa67e5608bec77930a
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-23 17:21:34 +0900

    [level 1] Title: 신고 결과 받기, Time: 211.83 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
@@ -1,27 +1,26 @@
 class Solution {
-    fun solution(idList: Array<String>, report: Array<String>, k: Int): IntArray {
-
-        val reportCountMap = mutableMapOf<String, Int>()
-        report.distinct().forEach {
-            val reportedUser = it.split(' ')[1]
-            reportCountMap[reportedUser] = reportCountMap.getOrDefault(reportedUser, 0) + 1
+    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+        // 신고 정보를 담을 Map을 생성
+        val reportMap = mutableMapOf<String, MutableSet<String>>()
+        // 결과 메일 수를 저장할 Map을 생성
+        val mailCount = mutableMapOf<String, Int>().apply {
+            id_list.forEach { this[it] = 0 }
         }
 
-        val suspendedUsers = reportCountMap.filterValues { it >= k }
-
-        val notificationsMap = mutableMapOf<String, Int>()
-        idList.forEach {
-            notificationsMap[it] = 0
+        // 신고 내역을 처리 (중복 신고 방지 위해 Set 사용)
+        for (r in report) {
+            val (from, to) = r.split(" ")
+            reportMap.getOrPut(to) { mutableSetOf() }.add(from)
         }
 
-        report.distinct().forEach {
-            val reporter = it.split(' ')[0]
-            val reportedUser = it.split(' ')[1]
-            if (suspendedUsers.contains(reportedUser)) {
-                notificationsMap[reporter] = notificationsMap.getOrDefault(reporter, 0) + 1
+        // 각 유저가 신고된 횟수를 확인하여 k번 이상이면 신고한 사람에게 메일 발송
+        for ((reportedUser, reporters) in reportMap) {
+            if (reporters.size >= k) {
+                reporters.forEach { mailCount[it] = mailCount.getValue(it) + 1 }
             }
         }
 
-        return notificationsMap.values.toIntArray()
+        // id_list 순서대로 메일 발송 횟수를 배열로 변환
+        return id_list.map { mailCount[it] ?: 0 }.toIntArray()
     }
 }
