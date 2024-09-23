255fe97a7e06e6da3ccaa24e1f30baf04736cbaf
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-23 17:27:15 +0900

    [level 1] Title: 이름이 있는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
+++ 프로그래머스/1/59407. 이름이 있는 동물의 아이디/이름이 있는 동물의 아이디.sql
@@ -1,4 +1,9 @@
-SELECT ANIMAL_ID
-FROM ANIMAL_INS
-WHERE NAME IS NOT NULL
-ORDER BY ANIMAL_ID;
+-- 이름이 있는 동물의 ID를 오름차순으로 조회하는 SQL문
+SELECT 
+    ANIMAL_ID  -- 동물의 ID를 선택
+FROM 
+    ANIMAL_INS  -- ANIMAL_INS 테이블에서
+WHERE 
+    NAME IS NOT NULL  -- 이름이 NULL이 아닌 경우만 선택
+ORDER BY 
+    ANIMAL_ID ASC;  -- 동물의 ID를 기준으로 오름차순 정렬
