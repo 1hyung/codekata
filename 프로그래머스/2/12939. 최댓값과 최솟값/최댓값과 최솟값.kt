11bef836a5ef9fed3805b44528449be6f3227e16
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-24 20:38:51 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 29.36 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
@@ -1,8 +1,13 @@
 class Solution {
     fun solution(s: String): String {
+        // 1. 문자열을 공백으로 나누고, 각 요소를 정수로 변환한 리스트 생성
         val numbers = s.split(" ").map { it.toInt() }
-        val min = numbers.minOf { it }
-        val max = numbers.maxOf { it }
-        return "$min $max"
+        
+        // 2. 최소값과 최대값 구하기
+        val minValue = numbers.minOrNull()
+        val maxValue = numbers.maxOrNull()
+
+        // 3. 최소값과 최대값을 "(최소값) (최대값)" 형태로 반환
+        return "$minValue $maxValue"
     }
 }
