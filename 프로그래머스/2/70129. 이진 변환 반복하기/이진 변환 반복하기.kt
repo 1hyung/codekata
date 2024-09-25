a53afff77844c878dcb2d5442c81cc9afd2e9066
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-26 00:08:23 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 35.89 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.kt
@@ -1,13 +1,26 @@
 class Solution {
     fun solution(s: String): IntArray {
-        val answer = intArrayOf(0, 0)
-        var str = s
+        var transformationCount = 0 // 이진 변환의 횟수를 저장
+        var removedZeroCount = 0    // 제거된 0의 개수를 저장
+
+        var currentString = s
         
-        while (str != "1") {
-            answer[0]++
-            answer[1] += str.count { it == '0' }
-            str = str.replace("0", "").length.toString(2)
+        // 문자열이 "1"이 될 때까지 반복
+        while (currentString != "1") {
+            // 1. 현재 문자열에서 0을 모두 제거
+            val zeroCount = currentString.count { it == '0' }
+            removedZeroCount += zeroCount
+
+            // 2. 0을 제거한 후 남은 문자열의 길이를 이진법으로 변환
+            currentString = currentString.replace("0", "")
+            val length = currentString.length
+            currentString = length.toString(radix = 2) // 길이를 2진수 문자열로 변환
+
+            // 3. 변환 횟수 증가
+            transformationCount++
         }
-        return answer
+        
+        // 변환 횟수와 제거된 0의 개수를 배열로 반환
+        return intArrayOf(transformationCount, removedZeroCount)
     }
 }
