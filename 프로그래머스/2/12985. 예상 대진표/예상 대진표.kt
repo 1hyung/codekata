9753090b518ca3cd229128387779e5f121234868
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-29 00:44:06 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -1,13 +1,19 @@
 class Solution {
-    fun solution(n: Int, aa: Int, bb: Int): Int {
-        var a = aa
-        var b = bb
-        var turn = 0
-        while (a != b) {
-            a = (a + 1) / 2
-            b = (b + 1) / 2
-            turn++
+    fun solution(n: Int, a: Int, b: Int): Int {
+        var round = 0  // 현재 라운드를 저장
+        var playerA = a  // A 참가자 번호
+        var playerB = b  // B 참가자 번호
+
+        // 두 참가자가 같은 번호를 가질 때까지 반복
+        while (playerA != playerB) {
+            // 라운드 수 증가
+            round++
+            
+            // 참가자 번호 갱신: 다음 라운드에서 이기는 사람의 번호는 (번호 + 1) / 2
+            playerA = (playerA + 1) / 2
+            playerB = (playerB + 1) / 2
         }
-        return turn
+
+        return round  // 두 참가자가 만나게 된 라운드 반환
     }
 }
