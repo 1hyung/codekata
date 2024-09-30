ad049e329cafc58c962dce5134c7459002515296
1hyung <161712242+1hyung@users.noreply.github.com>
2024-09-30 17:27:18 +0900

    [level 2] Title: N개의 최소공배수, Time: 16.42 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun solution(arr: IntArray): Int {
-        var answer = arr[0]
-        arr.forEach { answer = lcm(answer, it) }
-        return answer
-    }
-
-    fun lcm(a: Int, b: Int) = a * b / gcd(a, b)
-
-    fun gcd(a: Int, b: Int): Int {
-        return if (a < b) {
-            if (a == 0) b else gcd(a, b % a)
-        } else {
-            if (b == 0) a else gcd(b, a % b)
+        // 최소공배수를 구하기 위한 gcd 함수 정의
+        fun gcd(a: Int, b: Int): Int {
+            return if (b == 0) a else gcd(b, a % b)
         }
+
+        // 두 수의 최소공배수를 구하는 함수 정의
+        fun lcm(a: Int, b: Int): Int {
+            return (a * b) / gcd(a, b)
+        }
+
+        // 배열의 최소공배수를 구하기 위해 reduce 함수 사용
+        return arr.reduce { acc, num -> lcm(acc, num) }
     }
 }
