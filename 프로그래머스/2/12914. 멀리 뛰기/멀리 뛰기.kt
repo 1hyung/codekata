273a18cb095a8ff74ee46b660b8255cf5b8abc77
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-01 23:06:16 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
@@ -1,6 +1,20 @@
 class Solution {
-    fun solution(n: Int): Long {
-        var answer: Long = 0
-        return answer
+    fun solution(n: Int): Int {
+        // 기본적으로 배열로 DP 테이블을 선언합니다.
+        val dp = IntArray(n + 1)
+
+        // 기저 조건 설정
+        dp[1] = 1
+        if (n > 1) {
+            dp[2] = 2
+        }
+
+        // 점화식을 통해 DP 배열을 채웁니다.
+        for (i in 3..n) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+        }
+
+        // 마지막 dp[n]의 값을 반환합니다.
+        return dp[n]
     }
 }
