86ca8cdf5a6ffd0128dcf894cf8eab474a193be4
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-04 17:37:35 +0900

    [level 2] Title: H-Index, Time: 28.60 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.kt
+++ 프로그래머스/2/42747. H－Index/H－Index.kt
@@ -1,13 +1,18 @@
 class Solution {
     fun solution(citations: IntArray): Int {
+        // 1. 논문의 인용 횟수를 내림차순으로 정렬합니다.
         val sortedCitations = citations.sortedDescending()
-        
+
+        // 2. H-Index 계산
+        var hIndex = 0
         for (i in sortedCitations.indices) {
-            if (sortedCitations[i] <= i) {
-                return i
+            if (sortedCitations[i] >= i + 1) {
+                hIndex = i + 1
+            } else {
+                break
             }
         }
-        
-        return sortedCitations.size
+
+        return hIndex
     }
 }
