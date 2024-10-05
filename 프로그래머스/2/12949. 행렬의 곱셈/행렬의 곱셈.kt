a70cd9d73f5786af4737a8a0934e895e63ab3cc7
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-06 08:03:30 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.06 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
@@ -1,13 +1,23 @@
 class Solution {
     fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-        return Array(arr1.size) { i ->
-            IntArray(arr2[0].size) { j ->
-                var value = 0
-                for (k in arr2.indices) {
-                    value += arr1[i][k] * arr2[k][j]
+        val rowA = arr1.size // arr1의 행 개수
+        val colA = arr1[0].size // arr1의 열 개수
+        val colB = arr2[0].size // arr2의 열 개수
+
+        // 결과 행렬을 초기화 (크기는 rowA x colB)
+        val result = Array(rowA) { IntArray(colB) }
+
+        // 행렬 곱셈
+        for (i in 0 until rowA) { // arr1의 각 행에 대해 반복
+            for (j in 0 until colB) { // arr2의 각 열에 대해 반복
+                var sum = 0
+                for (k in 0 until colA) { // arr1의 열 개수이자 arr2의 행 개수만큼 반복
+                    sum += arr1[i][k] * arr2[k][j]
                 }
-                value
+                result[i][j] = sum // 결과 행렬에 값 저장
             }
         }
+
+        return result
     }
 }
