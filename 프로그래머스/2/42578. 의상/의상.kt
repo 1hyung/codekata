ae7ef66d79286458f9d0c6f47da292d103ad0c46
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-08 02:09:48 +0900

    [level 2] Title: 의상, Time: 0.20 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.kt
+++ 프로그래머스/2/42578. 의상/의상.kt
@@ -1,11 +1,21 @@
 class Solution {
     fun solution(clothes: Array<Array<String>>): Int {
-        val hashMap: HashMap<String, Int> = HashMap()
-        
+        // 의상의 종류별로 개수를 저장할 맵
+        val clothMap = mutableMapOf<String, Int>()
+
+        // 의상을 종류별로 개수 세기
         for (cloth in clothes) {
-            hashMap[cloth[1]] = hashMap.getOrDefault(cloth[1], 0) + 1
+            val kind = cloth[1]
+            clothMap[kind] = clothMap.getOrDefault(kind, 0) + 1
         }
-        
-        return hashMap.values.fold(1) { total, num -> total * (num + 1) } - 1
+
+        // 경우의 수 계산
+        var combinations = 1
+        for ((_, count) in clothMap) {
+            combinations *= (count + 1)
+        }
+
+        // 모든 의상을 입지 않은 경우를 제외하기 위해 1을 뺍니다.
+        return combinations - 1
     }
 }
