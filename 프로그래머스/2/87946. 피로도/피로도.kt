e8c96babf97e16f5827d75461c06961b119b5b68
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-10 01:54:50 +0900

    [level 2] Title: 피로도, Time: 4.28 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.kt
+++ 프로그래머스/2/87946. 피로도/피로도.kt
@@ -1,25 +1,28 @@
+import kotlin.math.max
+
 class Solution {
-
-    private lateinit var visited: BooleanArray
-    private var maxCount = 0
-
+    // 최대 탐험 가능한 던전 수를 계산하는 함수
     fun solution(k: Int, dungeons: Array<IntArray>): Int {
-        visited = BooleanArray(dungeons.size)
-        dfs(k, 0, dungeons)
-        return maxCount
+        return permute(dungeons, k, dungeons.size)
     }
 
-    private fun dfs(k: Int, count: Int, dungeons: Array<IntArray>) {
-        if (count > maxCount) {
-            maxCount = count
-        }
+    // 순열을 통해 모든 경우의 수를 탐색하는 함수
+    private fun permute(dungeons: Array<IntArray>, initialK: Int, n: Int): Int {
+        val used = BooleanArray(n) { false }
+        return dfs(dungeons, used, initialK, 0, 0)
+    }
 
+    // 깊이 우선 탐색을 통해 가능한 던전의 수를 계산하는 함수
+    private fun dfs(dungeons: Array<IntArray>, used: BooleanArray, k: Int, depth: Int, count: Int): Int {
+        var maxCount = count
         for (i in dungeons.indices) {
-            if (!visited[i] && dungeons[i][0] <= k) {
-                visited[i] = true
-                dfs(k - dungeons[i][1], count + 1, dungeons)
-                visited[i] = false
+            val (required, consumed) = dungeons[i]
+            if (!used[i] && k >= required) { // 아직 방문하지 않았고, 피로도가 충분한 경우
+                used[i] = true
+                maxCount = max(maxCount, dfs(dungeons, used, k - consumed, depth + 1, count + 1))
+                used[i] = false // 탐색이 끝난 후 다시 되돌리기
             }
         }
+        return maxCount
     }
 }
