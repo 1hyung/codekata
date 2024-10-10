e9b95d6bd6f175234e74dd2d82ab381f4b8da7f8
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-11 00:41:18 +0900

    [level 2] Title: 타겟 넘버, Time: 4.36 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,13 +1,20 @@
 class Solution {
-    fun find(numbers: IntArray, depth: Int, sum: Int, target: Int): Int {
-        if (depth == numbers.size) {
+    fun solution(numbers: IntArray, target: Int): Int {
+        return dfs(numbers, target, 0, 0)
+    }
+
+    private fun dfs(numbers: IntArray, target: Int, index: Int, sum: Int): Int {
+        // 모든 숫자를 사용한 경우
+        if (index == numbers.size) {
+            // 계산된 값이 타겟 넘버와 같으면 1을 반환
             return if (sum == target) 1 else 0
         }
-        return find(numbers, depth + 1, sum + numbers[depth], target) +
-               find(numbers, depth + 1, sum - numbers[depth], target)
-    }
-    
-    fun solution(numbers: IntArray, target: Int): Int {
-        return find(numbers, 0, 0, target)
+
+        // 현재 숫자를 더한 경우와 뺀 경우를 각각 재귀 호출
+        val add = dfs(numbers, target, index + 1, sum + numbers[index])
+        val subtract = dfs(numbers, target, index + 1, sum - numbers[index])
+
+        // 두 가지 경우의 수를 합산하여 반환
+        return add + subtract
     }
 }
