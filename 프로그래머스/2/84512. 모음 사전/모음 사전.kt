30c8828f91f1aece8ba54dd413cae99a72918c78
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-14 03:06:33 +0900

    [level 2] Title: 모음 사전, Time: 21.55 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.kt
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.kt
@@ -1,23 +1,16 @@
 class Solution {
-    val words = arrayOf('A', 'E', 'I', 'O', 'U')
-    var answer = 0 
-    var find = false 
-    
     fun solution(word: String): Int {
-        DFS("", word) 
-        
-        return answer 
-    }
-    
-    fun DFS(string: String, word: String) {
-        for (i in words) {
-            if (find) break 
-            
-            val s = string + i 
-            answer++ 
-            
-            if (s == word) find = true 
-            else if (s.length != 5) DFS(s, word) 
+        val vowels = listOf('A', 'E', 'I', 'O', 'U')  // 모음 리스트
+        val weight = intArrayOf(781, 156, 31, 6, 1)    // 각 자리별 가중치
+
+        var result = 0
+
+        for (i in word.indices) {
+            // 해당 문자가 위치한 인덱스 값을 찾고, 그 인덱스를 기반으로 가중치 계산
+            val idx = vowels.indexOf(word[i])
+            result += idx * weight[i] + 1  // 가중치 곱한 후 1을 더함 (현재 단어 포함)
         }
+
+        return result
     }
 }
