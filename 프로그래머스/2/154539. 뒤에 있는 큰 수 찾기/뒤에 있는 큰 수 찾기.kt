d93e246d50825f6fdd3016aeff33bb4ab23f6189
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-15 01:05:48 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 83.28 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.kt
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.kt
@@ -1,16 +1,18 @@
 class Solution {
     fun solution(numbers: IntArray): IntArray {
-        val answer: IntArray = IntArray(numbers.size) { -1 } 
-        val stack = mutableListOf<Int>() 
+        val result = IntArray(numbers.size) { -1 }  // 결과 배열을 -1로 초기화
+        val stack = mutableListOf<Int>()            // 스택 초기화
 
-        numbers.forEachIndexed { i, number -> 
-            while (stack.isNotEmpty() && number > numbers[stack.last()]) {
-                val topIndex = stack.removeLast() 
-                answer[topIndex] = number 
+        for (i in numbers.indices.reversed()) {     // 배열을 뒤에서부터 순회
+            while (stack.isNotEmpty() && stack.last() <= numbers[i]) {
+                stack.removeAt(stack.size - 1)      // 스택에서 현재 숫자보다 작거나 같은 숫자를 제거
             }
-            stack.add(i) 
+            if (stack.isNotEmpty()) {
+                result[i] = stack.last()            // 뒷 큰수를 찾은 경우 결과 배열에 기록
+            }
+            stack.add(numbers[i])                   // 현재 숫자를 스택에 추가
         }
 
-        return answer 
+        return result
     }
 }
