047f20665b07f6cfcaa544feb1b1ebbbbe37c09e
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-16 00:53:01 +0900

    [level 2] Title: 롤케이크 자르기, Time: 255.97 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
@@ -1,26 +1,32 @@
 class Solution {
     fun solution(topping: IntArray): Int {
-        var answer = 0
-        val arr1 = IntArray(topping.size) { 0 } 
-        val arr2 = IntArray(topping.size) { 0 } 
-        val menuCount = mutableSetOf<Int>()
+        val leftSet = mutableSetOf<Int>()           // 왼쪽 토핑 종류를 저장하는 집합
+        val rightMap = mutableMapOf<Int, Int>()     // 오른쪽 토핑을 종류별로 카운트하는 맵
 
-        for (i in 0 until topping.size) {
-            menuCount.add(topping[i])
-            arr1[i] = menuCount.size
+        // 처음에 오른쪽 맵을 모두 채운다
+        for (top in topping) {
+            rightMap[top] = rightMap.getOrDefault(top, 0) + 1
         }
 
-        menuCount.clear()
+        var count = 0
 
-        for (i in topping.size - 1 downTo 0) {
-            menuCount.add(topping[i])
-            arr2[i] = menuCount.size
+        // 왼쪽부터 순차적으로 탐색하며 자를 위치를 찾는다
+        for (i in topping.indices) {
+            // 왼쪽에 토핑 추가
+            leftSet.add(topping[i])
+
+            // 오른쪽에서 해당 토핑을 하나 제거
+            rightMap[topping[i]] = rightMap[topping[i]]!! - 1
+            if (rightMap[topping[i]] == 0) {
+                rightMap.remove(topping[i])
+            }
+
+            // 왼쪽과 오른쪽의 토핑 종류 수가 같으면 카운트 증가
+            if (leftSet.size == rightMap.size) {
+                count++
+            }
         }
 
-        for (i in 0 until topping.size - 1) {
-            if (arr1[i] == arr2[i + 1]) answer++
-        }
-
-        return answer
+        return count
     }
 }
