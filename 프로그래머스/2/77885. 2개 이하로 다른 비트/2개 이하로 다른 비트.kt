de997b875b6e46f4384048421769a1cedfebb2b9
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-18 00:18:09 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 28.00 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.kt
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.kt
@@ -1,14 +1,17 @@
 class Solution {
     fun solution(numbers: LongArray): LongArray {
-        return numbers.map {
-            if (it % 2 == 0L) { 
-                it + 1 
-            } else { 
-                val binary = it.toString(2).reversed() + "0" 
-                val index = binary.indexOf('0') 
-                val modifiedBinary = binary.substring(0, index - 1) + "01" + binary.substring(index + 1)
-                modifiedBinary.reversed().toLong(2) 
-            }
-        }.toLongArray() 
+        return numbers.map { f(it) }.toLongArray()
+    }
+    
+    private fun f(x: Long): Long {
+        if (x % 2 == 0L) {
+            // 짝수일 경우, x + 1이 바로 답
+            return x + 1
+        } else {
+            // 홀수일 경우
+            // 마지막 연속된 1 중 첫 번째 0을 1로 바꾸고, 그 다음 비트를 0으로 바꾼다
+            val bit = x or (x + 1)
+            return bit - ((bit - x) / 2)
+        }
     }
 }
