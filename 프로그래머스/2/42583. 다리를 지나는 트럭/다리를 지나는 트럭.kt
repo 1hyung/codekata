a2c4339fa1ab025cd67bc0f1934f3f4ecaffc2a4
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-19 09:31:17 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 20.66 ms, Memory: 60.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -1,27 +1,30 @@
-import java.util.*
+import java.util.LinkedList
+import java.util.Queue
 
 class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
-        var answer = 0
+        val bridge: Queue<Pair<Int, Int>> = LinkedList()  // 다리 위에 있는 트럭 (무게, 들어간 시간)
+        var time = 0  // 경과 시간
+        var currentWeight = 0  // 현재 다리 위의 트럭들의 무게 합
 
-        val bridgeQueue: Queue<Int> = LinkedList<Int>()
-        val waitQueue: Queue<Int> = LinkedList<Int>()
-
-        for (i in 0 until bridge_length) bridgeQueue.add(0)
-        for (i in truck_weights) waitQueue.add(i)
-
-        while (bridgeQueue.isNotEmpty()) {
-            answer++
-            bridgeQueue.poll() 
+        var i = 0  // 대기 중인 트럭의 인덱스
+        
+        while (i < truck_weights.size || bridge.isNotEmpty()) {
+            time++  // 1초가 경과
             
-            if (waitQueue.isNotEmpty()) {
-                if (bridgeQueue.sum() + waitQueue.peek() <= weight) {
-                    bridgeQueue.add(waitQueue.poll()) 
-                } else {
-                    bridgeQueue.add(0) 
-                }
+            // 시간이 지나 다리를 다 건넌 트럭은 큐에서 제거
+            if (bridge.isNotEmpty() && time - bridge.peek().second == bridge_length) {
+                currentWeight -= bridge.poll().first
+            }
+            
+            // 대기 트럭이 있고, 다리 위의 트럭 무게 합이 제한을 넘지 않으면 트럭을 다리에 올림
+            if (i < truck_weights.size && currentWeight + truck_weights[i] <= weight) {
+                bridge.add(Pair(truck_weights[i], time))  // 트럭을 다리에 올리고 들어간 시간을 기록
+                currentWeight += truck_weights[i]  // 다리 위의 무게에 트럭의 무게를 더함
+                i++  // 다음 대기 트럭으로 이동
             }
         }
-        return answer
+        
+        return time
     }
 }
