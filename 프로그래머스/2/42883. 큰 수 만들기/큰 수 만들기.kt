eb0cd32256b6c920e71fea1d0fbf527e9e09032e
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-24 17:59:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 90.20 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.kt
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.kt
@@ -1,19 +1,24 @@
 class Solution {
     fun solution(number: String, k: Int): String {
-        var answer = StringBuilder()  
-        var idx = 0  
-        
-        for (i in 0 until number.length - k) {
-            var max = '0' 
-            for (j in idx..i + k) {
-                if (max < number[j]) {
-                    max = number[j]
-                    idx = j + 1  
-                }
+        val stack = mutableListOf<Char>() // 스택처럼 사용할 리스트
+        var removeCount = k // 제거할 수 있는 횟수
+
+        for (digit in number) {
+            // 스택이 비어 있지 않고, 제거할 기회가 남아있으며, 스택의 마지막 숫자보다 현재 숫자가 크다면 스택에서 제거
+            while (stack.isNotEmpty() && removeCount > 0 && stack.last() < digit) {
+                stack.removeAt(stack.size - 1) // 마지막 요소 제거
+                removeCount--
             }
-            answer.append(max)  
+            stack.add(digit) // 현재 숫자를 스택에 추가
         }
-        
-        return answer.toString()  
+
+        // 제거할 기회가 남아있다면, 뒤에서부터 제거
+        while (removeCount > 0) {
+            stack.removeAt(stack.size - 1)
+            removeCount--
+        }
+
+        // 스택을 문자열로 변환하여 반환
+        return stack.joinToString("")
     }
 }
