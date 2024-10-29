291311157181c6495928846681c1b6b5dc8aa2de
1hyung <161712242+1hyung@users.noreply.github.com>
2024-10-29 19:00:23 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 33.21 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.kt
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.kt
@@ -1,68 +1,42 @@
 class Solution {
-    fun min(a: Int, b: Int): Int {
-        return if (a >= b) b else a
-    }
-    
-    fun solution(rows: Int, columns: Int, queries: Array<IntArray>): IntArray {
-        var answer = intArrayOf()
-        val array = Array(rows) { Array(columns) { 0 } }
-        var cnt = 1
-        
-        for (row in 0 until rows) {
-            for (col in 0 until columns) {
-                array[row][col] = cnt
-                cnt += 1
-            }
-        }
-        
+    fun solution(rows: Int, columns: Int, queries: Array<IntArray>): List<Int> {
+        // 1. 행렬 초기화
+        val matrix = Array(rows) { i -> IntArray(columns) { j -> i * columns + j + 1 } }
+        val result = mutableListOf<Int>() // 각 회전에서의 최솟값을 저장할 리스트
+
+        // 2. 테두리 회전을 수행
         for (query in queries) {
-            val srt = Pair(query[0] - 1, query[1] - 1) 
-            val dst = Pair(query[2] - 1, query[3] - 1) 
-            val element = arrayListOf<Int>()
-            var minValue = Int.MAX_VALUE
-            
-            for (j in srt.second until dst.second) {
-                element.add(array[srt.first][j])
-                minValue = min(minValue, array[srt.first][j])
+            val (x1, y1, x2, y2) = query.map { it - 1 } // 1부터 시작하므로 0 기반으로 변환
+            var minVal = Int.MAX_VALUE // 회전 중 가장 작은 값을 저장할 변수
+
+            // 테두리 회전 시작
+            var prev = matrix[x1][y1] // 테두리 첫 값을 저장
+
+            // 왼쪽 테두리 이동
+            for (x in x1 until x2) {
+                matrix[x][y1] = matrix[x + 1][y1].also { minVal = minOf(minVal, it) }
             }
-            
-            for (j in srt.first until dst.first) {
-                element.add(array[j][dst.second])
-                minValue = min(minValue, array[j][dst.second])
+
+            // 하단 테두리 이동
+            for (y in y1 until y2) {
+                matrix[x2][y] = matrix[x2][y + 1].also { minVal = minOf(minVal, it) }
             }
-            
-            for (j in dst.second downTo srt.second + 1) {
-                element.add(array[dst.first][j])
-                minValue = min(minValue, array[dst.first][j])
+
+            // 오른쪽 테두리 이동
+            for (x in x2 downTo x1 + 1) {
+                matrix[x][y2] = matrix[x - 1][y2].also { minVal = minOf(minVal, it) }
             }
-            
-            for (j in dst.first downTo srt.first + 1) {
-                element.add(array[j][srt.second])
-                minValue = min(minValue, array[j][srt.second])
+
+            // 상단 테두리 이동
+            for (y in y2 downTo y1 + 1) {
+                matrix[x1][y] = matrix[x1][y - 1].also { minVal = minOf(minVal, it) }
             }
-            
-            answer = answer.plus(minValue)
-            
-            val temp = element.removeAt(element.size - 1)
-            element.add(0, temp)
-            
-            var index = 0
-            for (j in srt.second until dst.second) {
-                array[srt.first][j] = element[index++]
-            }
-            
-            for (j in srt.first until dst.first) {
-                array[j][dst.second] = element[index++]
-            }
-            
-            for (j in dst.second downTo srt.second + 1) {
-                array[dst.first][j] = element[index++]
-            }
-            
-            for (j in dst.first downTo srt.first + 1) {
-                array[j][srt.second] = element[index++]
-            }
+
+            matrix[x1][y1 + 1] = prev // 첫 번째 값은 마지막 위치로 이동
+            minVal = minOf(minVal, prev) // 마지막으로 첫 번째 값 확인
+            result.add(minVal) // 현재 회전에서의 최솟값을 결과 리스트에 추가
         }
-        return answer
+        
+        return result
     }
 }
