7f9003fcd530e93c22e84e1986a8527b0ab26aa7
1hyung <161712242+1hyung@users.noreply.github.com>
2024-11-02 06:10:38 +0900

    [level 2] Title: 점 찍기, Time: 15.43 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.kt
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.kt
@@ -1,16 +1,20 @@
+import kotlin.math.sqrt
+
 class Solution {
     fun solution(k: Int, d: Int): Long {
-        var answer = 0L
-        val kLong = k.toLong()
-        val dLong = d.toLong()
-        val dSquared = dLong * dLong
-        
-        for (a in 0..dLong step kLong) {
-            val bb = dSquared - a * a  
-            val b = Math.sqrt(bb.toDouble()).toLong() / kLong + 1  
-            answer += b
+        var count = 0L
+        val maxDistanceSquared = d.toLong() * d.toLong() // d^2 계산
+
+        // x 좌표를 k의 배수로 증가시킴
+        for (x in 0..d step k) {
+            val xSquared = x.toLong() * x.toLong()
+            val maxYSquared = maxDistanceSquared - xSquared
+            
+            // 가능한 y의 최대값을 계산하고, k의 배수로 가능한 점 개수를 계산
+            val maxY = sqrt(maxYSquared.toDouble()).toLong()
+            count += (maxY / k) + 1
         }
-        
-        return answer
+
+        return count
     }
 }
