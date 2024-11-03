dd2970ae581c6a9f6eb059eced0376184419788c
1hyung <161712242+1hyung@users.noreply.github.com>
2024-11-03 22:04:07 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.kt
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.kt
@@ -1,34 +1,23 @@
 class Solution {
     fun solution(storey: Int): Int {
-        return solve(storey)
-    }
-
-    private fun solve(storey: Int): Int {
-        var tempStorey = storey
-        var cnt = 0
+        var remaining = storey
+        var moves = 0
         
-        while (tempStorey > 0) {
-            val ext = tempStorey % 10
-            tempStorey /= 10
+        while (remaining > 0) {
+            val lastDigit = remaining % 10 // 현재 자릿수의 값
+            val nextDigit = (remaining / 10) % 10 // 다음 자릿수의 값
             
-            cnt += when {
-                ext == 5 -> {
-                    if (tempStorey % 10 >= 5) {
-                        tempStorey++
-                        10 - ext
-                    } else {
-                        ext
-                    }
-                }
-                ext > 5 -> {
-                    tempStorey++
-                    10 - ext
-                }
-                else -> {
-                    ext
-                }
+            // 올림하는 게 더 유리한지 확인
+            if (lastDigit > 5 || (lastDigit == 5 && nextDigit >= 5)) {
+                moves += 10 - lastDigit
+                remaining += 10 - lastDigit // 윗자리로 올림 처리
+            } else {
+                moves += lastDigit // 내림으로 해결
             }
+            
+            remaining /= 10 // 다음 자릿수로 이동
         }
-        return cnt
+        
+        return moves
     }
 }
