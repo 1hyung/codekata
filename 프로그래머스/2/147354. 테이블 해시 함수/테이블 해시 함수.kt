069c579df19316ae35586e1c853256cb13485c85
1hyung <161712242+1hyung@users.noreply.github.com>
2024-11-08 22:59:46 +0900

    [level 2] Title: 테이블 해시 함수, Time: 39.02 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.kt
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.kt
@@ -1,24 +1,18 @@
 class Solution {
     fun solution(data: Array<IntArray>, col: Int, row_begin: Int, row_end: Int): Int {
-        data.sortWith { o1, o2 -> 
-            if (o1[col - 1] == o2[col - 1]) o2[0] - o1[0] 
-            else o1[col - 1] - o2[col - 1] 
+        // col번째 컬럼 기준 오름차순 정렬, 값이 동일하면 첫 번째 컬럼 기준 내림차순 정렬
+        val sortedData = data.sortedWith(compareBy({ it[col - 1] }, { -it[0] }))
+
+        var hashValue = 0 // 해시 값을 저장할 변수
+
+        // row_begin부터 row_end까지의 각 행에 대해 S_i 값을 계산하여 XOR
+        for (i in row_begin..row_end) {
+            // S_i 계산: 각 컬럼의 값을 i로 나눈 나머지들의 합
+            val s_i = sortedData[i - 1].sumOf { it % i }
+            // 누적 XOR 계산
+            hashValue = hashValue xor s_i
         }
-        
-        val s = ArrayList<Int>()
-        for (i in row_begin - 1..row_end - 1) {
-            var temp = 0
-            for (j in data[i].indices) {
-                temp += data[i][j] % (i + 1)
-            }
-            s.add(temp)
-        }
-        
-        var result = s[0]
-        for (i in 1..s.lastIndex) {
-            result = result xor s[i]
-        }
-        
-        return result
+
+        return hashValue
     }
 }
