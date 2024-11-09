2e449cd3428041140c31215e2001da897a1d4f37
1hyung <161712242+1hyung@users.noreply.github.com>
2024-11-09 15:48:20 +0900

    [level 2] Title: 하노이의 탑, Time: 3.42 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.kt
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.kt
@@ -1,20 +1,22 @@
 class Solution {
     fun solution(n: Int): Array<IntArray> {
-        var answer = mutableListOf<IntArray>()
-        
-        fun hanoi(num: Int, start: Int, end: Int, via: Int) {
-            if (num == 1) { 
-                answer.add(intArrayOf(start, end))
-                return
-            }
+        val result = mutableListOf<IntArray>()
+        hanoi(n, 1, 3, 2, result)
+        return result.toTypedArray()
+    }
 
-            hanoi(num - 1, start, via, end)
-            answer.add(intArrayOf(start, end))
-            hanoi(num - 1, via, end, start)
+    // 재귀 함수: n개의 원판을 from 기둥에서 to 기둥으로 옮김
+    private fun hanoi(n: Int, from: Int, to: Int, aux: Int, result: MutableList<IntArray>) {
+        if (n == 1) {
+            // 원판이 1개일 때는 바로 from에서 to로 옮기기
+            result.add(intArrayOf(from, to))
+        } else {
+            // 1. n-1개의 원판을 from에서 aux로 옮기기
+            hanoi(n - 1, from, aux, to, result)
+            // 2. 가장 큰 원판을 from에서 to로 옮기기
+            result.add(intArrayOf(from, to))
+            // 3. n-1개의 원판을 aux에서 to로 옮기기
+            hanoi(n - 1, aux, to, from, result)
         }
-
-        hanoi(n, 1, 3, 2)
-
-        return answer.toTypedArray()
     }
 }
