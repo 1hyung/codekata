9f82908656900085d1b5da5b4512b48d7e543d8b
1hyung <161712242+1hyung@users.noreply.github.com>
2024-11-17 03:26:13 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,19 +1,21 @@
 SELECT 
-    A.APNT_NO, 
-    P.PT_NAME, 
-    A.PT_NO, 
-    A.MCDP_CD, 
-    D.DR_NAME, 
-    A.APNT_YMD
+    A.APNT_NO,         -- 진료 예약 번호
+    P.PT_NAME,         -- 환자 이름
+    A.PT_NO,           -- 환자 번호
+    A.MCDP_CD,         -- 진료 과목 코드 (CS: 흉부외과)
+    D.DR_NAME,         -- 의사 이름
+    A.APNT_YMD         -- 진료 예약 일시
 FROM 
     APPOINTMENT A
 JOIN 
-    PATIENT P ON A.PT_NO = P.PT_NO
+    PATIENT P 
+    ON A.PT_NO = P.PT_NO  -- 환자 번호를 기준으로 JOIN
 JOIN 
-    DOCTOR D ON A.MDDR_ID = D.DR_ID
+    DOCTOR D 
+    ON A.MDDR_ID = D.DR_ID  -- 의사 ID를 기준으로 JOIN
 WHERE 
-    A.APNT_YMD LIKE '2022-04-13%' 
-    AND A.APNT_CNCL_YN = 'N'
-    AND A.MCDP_CD = 'CS'
+    DATE(A.APNT_YMD) = '2022-04-13'  -- 2022년 4월 13일 예약
+    AND A.APNT_CNCL_YN = 'N'         -- 취소되지 않은 예약
+    AND A.MCDP_CD = 'CS'             -- 진료 과목이 흉부외과
 ORDER BY 
-    A.APNT_YMD ASC;
+    A.APNT_YMD ASC;                  -- 예약 일시 기준 오름차순 정렬
