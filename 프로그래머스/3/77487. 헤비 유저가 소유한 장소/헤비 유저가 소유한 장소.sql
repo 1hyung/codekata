894a93b95dfcbdddf4c9ea6fdef760cc616774d4
1hyung <161712242+1hyung@users.noreply.github.com>
2024-11-29 18:50:05 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,9 +1,19 @@
-SELECT P.ID, P.NAME, P.HOST_ID
-FROM PLACES P
-WHERE P.HOST_ID IN (
-    SELECT HOST_ID
-    FROM PLACES
-    GROUP BY HOST_ID
-    HAVING COUNT(*) >= 2
-)
-ORDER BY P.ID;
+SELECT 
+    P.ID,
+    P.NAME,
+    P.HOST_ID
+FROM 
+    PLACES P
+WHERE 
+    P.HOST_ID IN (
+        SELECT 
+            HOST_ID
+        FROM 
+            PLACES
+        GROUP BY 
+            HOST_ID
+        HAVING 
+            COUNT(*) >= 2 -- 두 개 이상의 공간을 등록한 호스트만 선택
+    )
+ORDER BY 
+    P.ID; -- ID 기준으로 오름차순 정렬
