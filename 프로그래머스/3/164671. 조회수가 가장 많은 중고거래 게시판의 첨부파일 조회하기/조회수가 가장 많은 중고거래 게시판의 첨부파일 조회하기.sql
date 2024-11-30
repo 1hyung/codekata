a125a7d346ec4372115d11af4e1df809cea1d24c
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-01 02:26:02 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,12 +1,12 @@
 SELECT 
-    CONCAT('/home/grep/src/', F.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) AS FILE_PATH
+    CONCAT('/home/grep/src/', F.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) AS FILE_PATH -- 첨부파일 경로 생성
 FROM 
-    USED_GOODS_FILE F
-JOIN 
     USED_GOODS_BOARD B
+JOIN 
+    USED_GOODS_FILE F
 ON 
-    F.BOARD_ID = B.BOARD_ID
+    B.BOARD_ID = F.BOARD_ID -- 게시글 ID 기준으로 두 테이블 조인
 WHERE 
-    B.VIEWS = (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD)
+    B.VIEWS = (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD) -- 최대 조회수를 가진 게시글 필터링
 ORDER BY 
-    F.FILE_ID DESC;
+    F.FILE_ID DESC; -- FILE_ID 기준으로 내림차순 정렬
