fc658d4c4b236475105ae9945f4e2b6c5de4eae5
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-06 21:03:27 +0900

    [level 2] Title: 유사 칸토어 비트열, Time: 0.11 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.kt
+++ 프로그래머스/2/148652. 유사 칸토어 비트열/유사 칸토어 비트열.kt
@@ -5,22 +5,22 @@ class Solution {
     fun solution(n: Int, l: Long, r: Long): Int {
         return count(n, r) - count(n, l - 1)
     }
-    
+
     private fun count(n: Int, k: Long): Int {
         if (n == 0) return 1
 
         val prevBitStringNumber = n - 1
-        val divisor = 5.0.pow(prevBitStringNumber).toLong() // 5등분으로 나누는 수
-        val numberOf1s = 4.0.pow(prevBitStringNumber).toInt() // 이전 비트열 1의 개수
+        val divisor = 5.0.pow(prevBitStringNumber).toLong() // 각 구간의 길이
+        val numberOf1s = 4.0.pow(prevBitStringNumber).toInt() // 이전 비트열의 1의 개수
 
-        var zone = abs(k / divisor).toInt()   // k번째가 속한 구역(0, 1, 2, 3, 4)
+        var zone = abs(k / divisor).toInt()   // k번째 위치가 속한 구간 인덱스 (0 ~ 4)
         if (k % divisor == 0L) zone -= 1
 
-        // 0만 있는 구역
+        // 0만 있는 구간
         if (zone == 2) return numberOf1s * zone
-        // 0 이후 구역
+        // 0 이후 구간
         if (zone > 2) return numberOf1s * (zone - 1) + count(n - 1, k - (divisor * zone))
-        // 0 이전 구역
+        // 0 이전 구간
         return numberOf1s * zone + count(n - 1, k - (divisor * zone))
     }
 }
