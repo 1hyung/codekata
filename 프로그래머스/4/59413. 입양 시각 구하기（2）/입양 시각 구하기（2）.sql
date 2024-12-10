87afd88f4eb32fb43234bb0f058835fc7a9912ad
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-10 17:21:57 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,9 +1,9 @@
 SET @hour := -1;
 
 SELECT 
-    (@hour := @hour + 1) AS HOUR,
-    (SELECT COUNT(*) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) AS COUNT
+    (@hour := @hour + 1) AS HOUR,                                -- 시간대 0~23 생성
+    (SELECT COUNT(*) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) AS COUNT -- 해당 시간대 입양 건수 집계
 FROM 
     ANIMAL_OUTS
 WHERE 
-    @hour < 23;
+    @hour < 23;                                                  -- 시간대가 23 이하일 때만 반복
