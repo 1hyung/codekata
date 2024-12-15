0fffeb79af635e79bc3d087068b046dd2105aa28
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-15 20:24:00 +0900

    [level 1] Title: 역순 정렬하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59035. 역순 정렬하기/역순 정렬하기.sql
+++ 프로그래머스/1/59035. 역순 정렬하기/역순 정렬하기.sql
@@ -1,3 +1,8 @@
-SELECT NAME, DATETIME
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID DESC;
+-- 동물 보호소에 들어온 모든 동물의 이름과 보호 시작일을 ANIMAL_ID 역순으로 조회하는 SQL문 
+SELECT 
+    NAME, -- 동물의 이름 
+    DATETIME -- 보호 시작일 선택
+FROM 
+    ANIMAL_INS -- 데이터를 조회할 테이블 선택
+ORDER BY
+    ANIMAL_ID DESC; -- ANIMAL_ID 역순 정렬
