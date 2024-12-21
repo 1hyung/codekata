4aae8fbc9a14c50d659eed88e0e38fd2128f64ea
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-21 23:04:59 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.kt
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.kt
@@ -1,4 +1,7 @@
-// 두 정수 num1과 num2를 입력받아, 두 수의 차를 계산하는 함수 
+// 두 정수의 차를 계산하여 반환하는 함수 
 class Solution {
-    fun solution(num1: Int, num2: Int): Int = num1 - num2
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = num1 - num2
+        return answer
+    }
 }
