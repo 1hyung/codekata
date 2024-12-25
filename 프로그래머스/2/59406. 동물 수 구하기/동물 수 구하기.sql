6ddc900e488b360fbf88315975d8eb94e3b5fcfb
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-25 18:56:07 +0900

    [level 2] Title: 동물 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59406. 동물 수 구하기/동물 수 구하기.sql
+++ 프로그래머스/2/59406. 동물 수 구하기/동물 수 구하기.sql
@@ -1,3 +1,5 @@
--- 코드를 입력하세요
-SELECT count(*) as count
-FROM ANIMAL_INS;
+-- 동물 보호소에 들어온 동물의 수를 조회
+SELECT
+    COUNT(ANIMAL_ID) AS count -- ANIMAL_ID을 세어 동물 수 계산
+FROM
+    ANIMAL_INS; -- 데이터를 조회할 테이블 
