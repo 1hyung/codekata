7e8b4bb6233c9a21fc4ffd639a37c6a3e944c9f4
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-26 23:10:56 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합/두 수의 합.kt
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.kt
@@ -1,5 +1,4 @@
+// 두 정수의 합을 계산하여 단일 표현식 함수로 간결하게 표현 
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        return num1 + num2
-    }
+    fun solution(num1: Int, num2: Int): Int = num1 + num2
 }
