1a666c9ed1980672acf1116fbab8eb0496b46a9d
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-27 23:09:24 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -1,5 +1,5 @@
+// 두 수를 나눈 결과에 1000을 곱하고 정수 부분을 반환하는 함수
+// num1에 1000을 곱한 뒤 num2로 나누면 매개변수가 Int이기 때문에 정수 반환
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        return num1 * 1000 / num2
-    }
+    fun solution(num1: Int, num2: Int): Int = num1 * 1000 / num2
 }
