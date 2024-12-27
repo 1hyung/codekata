a0ccd0d12b31550b713a3a470dc375d89ac3843e
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-27 23:53:36 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,4 +1,10 @@
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS 
-WHERE INTAKE_CONDITION = 'Sick'
-ORDER BY ANIMAL_ID;
+-- 동물 보호소에 들어온 동물 중 아픈 동물의 아이디와 이름을 아이디 순으로 조회하는 SQL 
+SELECT
+    ANIMAL_ID, -- 동물의 ID
+    NAME -- 동물의 이름
+FROM 
+    ANIMAL_INS -- 데이터를 조회할 테이블 
+WHERE 
+    INTAKE_CONDITION = 'Sick' -- 아픈 동물만 선택
+ORDER BY
+    ANIMAL_ID ASC; -- 아이디를 오름차순으로 정렬
