ca9016d294662f86000e0bd6c616788048128e10
1hyung <161712242+1hyung@users.noreply.github.com>
2024-12-28 23:20:13 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -1,11 +1,13 @@
+// 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류
+// 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수
 class Solution {
-    fun solution(angle: Int): Int {
-        return when{
-            angle in 1 until 90 -> 1 // 예각 (0 < angle < 90)
-            angle == 90 -> 2 // 직각 (angle = 90)
-            angle in 91 until 180 -> 3 // 둔각 (90 < angle < 180)
-            angle == 180 -> 4 // 평각 (angle = 180)
-            else -> throw IllegalArgumentException("Invalid angle") // 유효하지 않은 각도 처리
-        }
+    // 주어진 각도를 조건에 따라 분류하여 반환하는 함수 
+    fun solution(angle: Int): Int = 
+        when {
+            angle < 90 -> 1 //예각
+            angle == 90 -> 2 //직각
+            angle < 180 -> 3 //둔각
+            angle == 180 -> 4 //평각
+            else -> 0 // 예외 처리
     }
 }
