97680948d080adff650aa33232299ad4bd9f32e7
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-02 23:58:22 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.51 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -1,17 +1,7 @@
 class Solution {
-    fun solution(n: Int): Int {
-        // n을 문자열로 변환하고 각 자릿수를 정수로 변환하여 리스트로 만듭니다.
-        val digits = n.toString().map {
-            val digit = it.toInt() - '0'.toInt()
-            println("Digit: $digit") // 중간 값을 출력
-            digit
-        }
-
-        // 리스트의 숫자들을 모두 더합니다.
-        val sum = digits.sum()
-        println("Sum: $sum") // 합계를 출력
-        
-        // 합계를 반환합니다.
-        return sum
-    }
+    fun solution(n: Int): Int =
+        n.toString()               // 정수 n을 문자열로 변환하여 각 자릿수를 분리 (예: 123 → "123")
+            .sumOf { 
+                it.digitToInt()    // 각 문자를 정수로 변환 (예: '1' → 1)
+            }                     // 변환된 정수들의 합을 계산 sumOf
 }
