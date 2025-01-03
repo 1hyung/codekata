21fd7c5ac37449c98a446647ebf9ae28cb8de4f7
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-03 22:13:22 +0900

    [level 1] Title: 자릿수 더하기, Time: 1.46 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -1,7 +1,8 @@
 class Solution {
-    fun solution(n: Int): Int =
-        n.toString()               // 정수 n을 문자열로 변환하여 각 자릿수를 분리 (예: 123 → "123")
-            .sumOf { 
-                it.digitToInt()    // 각 문자를 정수로 변환 (예: '1' → 1)
-            }                     // 변환된 정수들의 합을 계산 sumOf
+    fun solution(n: Int): Int {
+        return n.toString() // 정수 n을 문자열로 변환하여 각 자릿수를 분리 (예: 123 → "123")
+            .sumOf {
+                it.digitToInt() // 각 문자를 정수로 변환 (예: '1' → 1)
+            } // 변환된 정수들의 합을 계산 sumOf
+    }
 }
