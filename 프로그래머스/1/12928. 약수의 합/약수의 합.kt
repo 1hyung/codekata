2332f40c6e5095c70616699d3d7e202b2b68dfa8
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-04 23:23:15 +0900

    [level 1] Title: 약수의 합, Time: 12.49 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.kt
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.kt
@@ -1,12 +1,5 @@
 class Solution {
-    // 반복문을 활용한 풀이
-    fun solution(n: Int): Int {
-        var sum = 0
-        for (i in 1..n) { // i가 1부터 n가지 반복
-            if (n % i == 0) { // i가 n의 약수인지 확인
-                sum += i // 약수를 합산
-            }
-        }
-        return sum 
-    }
+    fun solution(n: Int): Int =
+        (1..n).filter { n % it == 0} // 약수 필터링
+              .sum() // 약수들의 합 계산
 }
