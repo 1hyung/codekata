2d72619a6daf54ef7805601582b9f962d055cfbc
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-11 22:51:09 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 22.60 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -1,15 +1,5 @@
 class Solution {
-    fun solution(a: Int, b: Int): Long {
-        // a와 b 중 작은 값과 큰 값을 결정합니다.
-        val start = minOf(a, b)
-        val end = maxOf(a, b)
-        
-        // 작은 값부터 큰 값까지의 합을 계산합니다.
-        var sum: Long = 0
-        for (i in start..end) {
-            sum += i
-        }
-        
-        return sum
-    }
+    fun solution(a: Int, b: Int): Long =
+         (minOf(a, b)..maxOf(a, b)) // a와 b 사이의 범위를 생성 (작은 값부터 큰 값까지)
+            .sumOf{ it.toLong() }  // 각 정수를 Long으로 변환하여 합산
 }
