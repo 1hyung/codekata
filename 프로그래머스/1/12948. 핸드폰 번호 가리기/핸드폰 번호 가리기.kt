997e041189c08cf3638ec1be746fedabb6827141
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-16 23:51:51 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 16.31 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,8 +1,7 @@
 class Solution {
+    // 전화번호의 일부를 '*'로 가리고 반환하는 함수
     fun solution(phone_number: String): String {
-        val length = phone_number.length
-        val maskedPart = "*".repeat(length - 4)
-        val lastFourDigits = phone_number.takeLast(4)
-        return maskedPart + lastFourDigits
+        return "*".repeat(phone_number.length - 4) + // '*'로 가린 부분
+            phone_number.takeLast(4) // 뒷 4자리와 결합하여 반환
     }
 }
