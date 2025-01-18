cfb6d442d23a44735b1a61b71cca71d9f3051cb7
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-18 21:40:47 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 605.61 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 제일 작은 수 제거하기 - 12935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=kotlin) 
 
 ### 성능 요약
 
-메모리: 81.6 MB, 시간: 28.95 ms
+메모리: 83.6 MB, 시간: 605.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 10일 22:57:46
+2025년 01월 18일 21:40:43
 
 ### 문제 설명
 
--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.kt
@@ -1,16 +1,5 @@
 class Solution {
-    fun solution(arr: IntArray): IntArray {
-        // 배열이 비어있는 경우 또는 길이가 1인 경우, [-1]을 반환합니다.
-        if (arr.size <= 1) {
-            return intArrayOf(-1)
-        }
-        
-        // 가장 작은 값을 찾습니다.
-        val minValue = arr.minOrNull()
-        
-        // 가장 작은 값을 제외한 새로운 배열을 만듭니다.
-        val result = arr.filter { it != minValue }.toIntArray()
-        
-        return result
-    }
+    fun solution(arr: IntArray): IntArray =
+        if (arr.size == 1) intArrayOf(-1) // 배열이 한 개의 요소만 가지는 경우 [-1] 반환
+        else arr.filter { it != arr.minOrNull() }.toIntArray() // 가장 작은 값을 제외하고 반환
 }
