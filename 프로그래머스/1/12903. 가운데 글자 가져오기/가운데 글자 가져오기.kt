13d07f02b1bc9c4676f6d8b0bc76f0e70970e1d0
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-19 22:46:14 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
@@ -1,12 +1,7 @@
 class Solution {
-    fun solution(s: String): String {
-        val len = s.length
-        return if (len % 2 == 0) {
-            // 문자열의 길이가 짝수인 경우, 가운데 두 글자를 반환
-            s.substring(len / 2 - 1, len / 2 + 1)
-        } else {
-            // 문자열의 길이가 홀수인 경우, 가운데 한 글자를 반환
-            s.substring(len / 2, len / 2 + 1)
-        }
-    }
+    fun solution(s: String): String =
+        if (s.length % 2 == 0) // 문자열 길이가 짝수인 경우
+            s.substring(s.length / 2 - 1, s.length / 2 + 1) // 가운데 두 글자
+        else // 문자열 길이가 홀수인 경우
+            s.substring(s.length / 2, s.length / 2 + 1) // 가운데 한 글자
 }
