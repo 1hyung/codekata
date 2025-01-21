08579d2493b6ca6d54c9c3ad37f8dfcc0ef086c1
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-21 22:37:55 +0900

    [level 1] Title: 내적, Time: 21.56 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.kt
+++ 프로그래머스/1/70128. 내적/내적.kt
@@ -1,5 +1,4 @@
 class Solution {
-    fun solution(a: IntArray, b: IntArray): Int {
-        return a.indices.sumOf { a[it] * b[it] }
-    }
+    fun solution(a: IntArray, b: IntArray): Int =
+        a.zip(b).sumOf { it.first * it.second } // 두 배열을 쌍으로 묶고 각 쌍의 곱을 더함
 }
