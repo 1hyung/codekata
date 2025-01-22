5c31e9eb4ad45e3913f1e8e2ac8ea1ebf4e82517
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-22 17:54:42 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.22 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,27 +1,10 @@
 class Solution {
-    fun solution(left: Int, right: Int): Int {
-        var result = 0
-
-        for (num in left..right) {
-            val divisorCount = countDivisors(num)
-            if (divisorCount % 2 == 0) {
-                result += num
-            } else {
-                result -= num
+    // 주어진 범위의 숫자를 순회하며 약수 개수에 따라 더하거나 뺀 결과를 반환하는 함수
+    fun solution(left: Int, right: Int): Int =
+        (left..right) // left부터 right까지의 범위 생성
+            .sumOf { n -> // 각 숫자의 결과를 계산하여 합산
+                val sqrt = Math.sqrt(n.toDouble()).toInt() // n의 제곱근을 계산하고 정수로 변환
+                if (sqrt * sqrt == n) -n // 제곱근의 제곱이 n과 같다면 제곱수 → 약수 개수는 홀수 → 음수 처리
+                else n // 그렇지 않으면 약수 개수는 짝수 → 양수 처리
             }
-        }
-
-        return result
-    }
-
-    // 주어진 숫자의 약수 개수를 구하는 함수
-    private fun countDivisors(num: Int): Int {
-        var count = 0
-        for (i in 1..num) {
-            if (num % i == 0) {
-                count++
-            }
-        }
-        return count
-    }
 }
