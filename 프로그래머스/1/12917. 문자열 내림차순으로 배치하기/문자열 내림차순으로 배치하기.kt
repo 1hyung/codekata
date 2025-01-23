19ba2516c6909509417b4b99c4646382f37026bb
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-23 23:31:48 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 45.39 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.kt
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.kt
@@ -1,5 +1,7 @@
 class Solution {
     fun solution(s: String): String {
-        return s.toCharArray().sortedDescending().joinToString("")
+        return s.toCharArray()     // 문자열을 문자 배열로 변환
+            .sortedDescending()    // 문자 배열을 내림차순으로 정렬 (대문자는 소문자보다 작은 것으로 간주)
+            .joinToString("")      // 정렬된 문자 배열을 빈 구분자로 결합하여 문자열로 변환
     }
 }
