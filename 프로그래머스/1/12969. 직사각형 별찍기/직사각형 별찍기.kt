44f8dd41f3edc081663b7d0799150fb0b311e300
1hyung <161712242+1hyung@users.noreply.github.com>
2025-01-27 23:38:48 +0900

    [level 1] Title: 직사각형 별찍기, Time: 214.57 ms, Memory: 50.1 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -1,12 +1,4 @@
 fun main(args: Array<String>) {
-    // 표준 입력으로 두 개의 정수 n과 m을 받습니다.
-    val (n, m) = readLine()!!.split(' ').map { it.toInt() }
-    
-    // 가로로 n개의 별을 가진 문자열을 만듭니다.
-    val row = "*".repeat(n)
-    
-    // 이 문자열을 m번 반복해서 출력합니다.
-    for (i in 1..m) {
-        println(row)
-    }
+    val (n, m) = readLine()!!.split(' ').map(String::toInt) // 입력값 처리
+    List(m) { "*".repeat(n) }.forEach { println(it) } // m개의 '*'로 이루어진 리스트 생성 후 출력
 }
