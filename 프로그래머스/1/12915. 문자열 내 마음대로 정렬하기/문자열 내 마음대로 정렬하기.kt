370e68aef9366abd26ff46daa25f88ae3542a4fb
1hyung <161712242+1hyung@users.noreply.github.com>
2025-02-05 22:26:04 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 22.84 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.kt
@@ -1,5 +1,8 @@
 class Solution {
     fun solution(strings: Array<String>, n: Int): Array<String> {
-        return strings.sortedWith(compareBy({ it[n] }, { it })).toTypedArray()
+        return strings.sortedWith(Comparator { a, b ->
+            if (a[n] == b[n]) a.compareTo(b) // n번째 문자가 같다면 사전순 정렬
+            else a[n].compareTo(b[n]) // n번째 문자를 기준으로 정렬
+        }).toTypedArray()
     }
 }
