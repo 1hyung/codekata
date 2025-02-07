c11604f60dd3d25f0cd568a7a7c8b66d965f2a0b
1hyung <161712242+1hyung@users.noreply.github.com>
2025-02-07 23:24:14 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,11 +1,20 @@
-WITH MaxPricePerCategory AS (
-    SELECT CATEGORY, MAX(PRICE) AS MAX_PRICE
+-- 특정 카테고리 내에서 가격이 가장 비싼 식품 정보 조회
+-- 가격(PRICE) 기준 내림차순 정렬
+SELECT 
+    f.CATEGORY, 
+    f.PRICE AS MAX_PRICE, 
+    f.PRODUCT_NAME
+FROM FOOD_PRODUCT f
+JOIN (
+    -- 각 카테고리별 최고 가격을 구하는 서브쿼리
+    SELECT 
+        CATEGORY, 
+        MAX(PRICE) AS MAX_PRICE
     FROM FOOD_PRODUCT
-    WHERE CATEGORY IN ('과자', '국', '김치', '식용유')
+    WHERE CATEGORY IN ('과자', '국', '김치', '식용유') -- 특정 카테고리 필터링
     GROUP BY CATEGORY
-)
-SELECT F.CATEGORY, M.MAX_PRICE, F.PRODUCT_NAME
-FROM FOOD_PRODUCT F
-JOIN MaxPricePerCategory M ON F.CATEGORY = M.CATEGORY AND F.PRICE = M.MAX_PRICE
-WHERE F.CATEGORY IN ('과자', '국', '김치', '식용유')
-ORDER BY M.MAX_PRICE DESC;
+) max_price_table
+ON f.CATEGORY = max_price_table.CATEGORY 
+AND f.PRICE = max_price_table.MAX_PRICE
+WHERE f.CATEGORY IN ('과자', '국', '김치', '식용유') -- 특정 카테고리 필터링
+ORDER BY f.PRICE DESC;
