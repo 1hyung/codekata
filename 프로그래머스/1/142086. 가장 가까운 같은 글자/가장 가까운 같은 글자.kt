bcc56ecf499dbe55ba8f90c571d7ea2e211af690
1hyung <161712242+1hyung@users.noreply.github.com>
2025-02-08 21:40:40 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 29.88 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.kt
@@ -1,18 +1,11 @@
 class Solution {
     fun solution(s: String): IntArray {
-        val lastSeen = mutableMapOf<Char, Int>()  // 각 문자의 마지막 출현 위치를 저장할 맵
-        val result = IntArray(s.length)  // 결과를 저장할 배열
+        val lastSeen = mutableMapOf<Char, Int>() // 각 문자의 마지막 위치 저장
 
-        for (i in s.indices) {
-            val char = s[i]
-            if (char in lastSeen) {
-                result[i] = i - lastSeen[char]!!
-            } else {
-                result[i] = -1
-            }
-            lastSeen[char] = i  // 현재 문자의 위치를 업데이트
-        }
-
-        return result
+        return s.mapIndexed { i, char ->
+            val distance = lastSeen[char]?.let { i - it } ?: -1 // 존재하면 거리 계산, 없으면 -1
+            lastSeen[char] = i // 현재 문자의 위치 업데이트
+            distance
+        }.toIntArray()
     }
 }
