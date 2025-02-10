91bfe4ead23124315b32c8bc6cadd297f98492a5
1hyung <161712242+1hyung@users.noreply.github.com>
2025-02-10 16:47:49 +0900

    [level 0] Title: 두 수의 곱, Time: 0.02 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.kt
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.kt
@@ -1,12 +1,4 @@
-// 두 정수의 곱
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        // 입력받은 두 수를 multiply 함수를 사용해 곱한 값을 반환
-        return multiply(num1, num2)
-    }
-
-    // 두 수의 곱을 반환하는 함수 (Solution 클래스 내부에서만 사용 가능)
-    private fun multiply(a: Int, b: Int): Int {
-        return a * b 
-    }
+    // 간결한 단일 표현식 함수 사용 
+    fun solution(num1: Int, num2: Int): Int = num1 * num2
 }
