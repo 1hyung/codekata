c9c9958cf39d8a712543586e5e20932333c3d99d
1hyung <161712242+1hyung@users.noreply.github.com>
2025-02-17 23:50:49 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.kt
@@ -1,14 +1,12 @@
 class Solution {
     fun solution(n: Int): Int {
-        var num = n // 입력값 n을 변수 num에 저장 (num은 자릿수를 분리하면서 값을 변경)
-        var sum = 0 // 각 자릿수의 합을 저장할 변수 sum 선언
+        var num = n // 입력값을 변경할 변수 
+        var sum = 0 // 합계를 저장할 변수 
         
-        while (num > 0) { // num이 0이 될 때까지 반복
-            sum += num % 10 // 1. num의 마지막 자릿수를 sum에 더 함
-            num /= 10 // 2. 마지막 자릿수를 제거 (num을 10으로 나눔)
+        while (num > 0) { // num가 0이 될 때까지 반복
+            sum += num % 10 // 1. 마지막 자릿수를 sum에 추가 
+            num /= 10 // 2. 마지막 자릿수를 제거
         }
-        
-        return sum // 자릿수의 합 반환
-
+        return sum // 최종 합 반환
     }
 }
