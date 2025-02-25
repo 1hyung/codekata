ca3cf3dc9623c240a97faea6e84901c78000d134
1hyung <161712242+1hyung@users.noreply.github.com>
2025-02-25 19:53:45 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 64.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -2,12 +2,13 @@
 // 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수
 class Solution {
     // 주어진 각도를 조건에 따라 분류하여 반환하는 함수 
-    fun solution(angle: Int): Int = 
-        when {
+    fun solution(angle: Int): Int {
+        return when {
             angle < 90 -> 1 //예각
             angle == 90 -> 2 //직각
             angle < 180 -> 3 //둔각
             angle == 180 -> 4 //평각
             else -> 0 // 예외 처리
+        }
     }
 }
