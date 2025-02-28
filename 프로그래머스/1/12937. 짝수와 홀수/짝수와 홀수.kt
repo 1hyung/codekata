f3fa361adc33bc7680c5c8663a7197996cbbb6b3
1hyung <161712242+1hyung@users.noreply.github.com>
2025-02-28 17:52:45 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.kt
@@ -1,4 +1,4 @@
-// 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 단일 표현식 함수
 class Solution {
+    // 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 단일 표현식 함수 
     fun solution(num: Int): String = if (num % 2 == 0) "Even" else "Odd"
 }
