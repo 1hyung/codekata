7889b9a797b8fce088260f32a93749a70f032ff9
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-05 23:22:32 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.04 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.kt
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.kt
@@ -1,3 +1,4 @@
 class Solution {
-    fun solution(s: String): Int = s.toInt() // 문자열을 정수로 변환, 코틀린은 자동으로 부호까지 처리
+    fun solution(s: String): Int = 
+        s.toInt() // 문자열을 정수로 변환, 코틀린은 자동으로 부호까지 처리
 }
