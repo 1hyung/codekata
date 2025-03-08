5561624df70f47b21bd9d869145346e07a9c7bd1
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-08 11:17:06 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.kt
@@ -1,7 +1,13 @@
 class Solution {
-    fun solution(x: Int): Boolean =
-        x % x.toString() // 정수 x를 문자열로 변환 (예: 123 → "123")
-            .sumOf { it.digitToInt() } == 0 
-    // 문자열의 각 문자를 정수로 변환한 뒤 합산 (예: "123" → 1 + 2 + 3 = 6)
-    // x가 자릿수 합으로 나누어 떨어지는지 확인 (예: 123 % 6 == 0)
+    fun solution(x: Int): Boolean {
+        var num = x
+        var sum = 0
+
+        while (num > 0) { // 모든 자릿수를 합산
+            sum += num % 10 // 마지막 자릿수를 더함
+            num /= 10 // 마지막 자릿수를 제거
+        }
+
+        return x % sum == 0 // 나누어 떨어지면 true 반환
+    }
 }
