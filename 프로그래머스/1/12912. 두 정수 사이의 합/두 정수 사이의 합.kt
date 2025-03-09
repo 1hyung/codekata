cfca2b8dfaf0a773e78a739e77e9f835283638c2
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-09 22:43:02 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 21.57 ms, Memory: 61.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -1,5 +1,5 @@
 class Solution {
     fun solution(a: Int, b: Int): Long =
          (minOf(a, b)..maxOf(a, b)) // a와 b 사이의 범위를 생성 (작은 값부터 큰 값까지)
-            .sumOf{ it.toLong() }  // 각 정수를 Long으로 변환하여 합산
+            .sumOf { it.toLong() }  // 각 정수를 Long으로 변환하여 합산
 }
