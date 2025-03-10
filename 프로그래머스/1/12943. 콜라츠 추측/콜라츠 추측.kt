f1a959d37b339164baff4236b313eef9b7083f3e
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-10 23:40:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.kt
@@ -1,15 +1,13 @@
 class Solution {
-    // 작업을 몇 번이나 반복해야 하는지 반환하는 함수
     fun solution(num: Int): Int {
-        var count = 0 // 작업 횟수
-        var current = num.toLong() // 정수형 overflow 방지를 위해 Long 사용
+        var current = num.toLong()
+        var count = 0
         
-        while (current != 1L) { // 1이 될 때까지 반복
-            if (count == 500) return -1 // 500번 이상 반복 시 -1 반환
-            current = if (current % 2 == 0L) current / 2 else current * 3 + 1 // 짝수 / 홀수 조건 처리
-            count ++ // 작업 횟수 증가
-            
+        while (current != 1L && count < 500) { // 1이 되거나 500번 넘으면 종료
+            current = if (current % 2 == 0L) current / 2 else current * 3 + 1
+            count++
         }
-        return count // 작업 횟수 반환 
+        
+        return if (count < 500) count else -1 // 500번 넘었으면 -1 반환
     }
 }
