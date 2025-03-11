af244378e146150de74ed191ae75f2d5fb00eb4e
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-11 22:12:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 31.71 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -1,6 +1,4 @@
 class Solution {
-    fun solution(seoul: Array<String>): String {
-        val position = seoul.indexOf("Kim") // "Kim"의 위치를 찾음
-        return "김서방은 ${position}에 있다" // 위치를 문자열 템플릿으로 반환
-    }
+    fun solution(seoul: Array<String>): String =
+        "김서방은 ${seoul.indexOf("Kim")}에 있다" // indexOf를 직접 문자열에 적용
 }
