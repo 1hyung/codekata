3d9b340b5daa63c3f9c4a36c2ec34f6eb1371bcb
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-14 23:37:48 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 15.07 ms, Memory: 65 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,7 +1,4 @@
 class Solution {
-    // 전화번호의 일부를 '*'로 가리고 반환하는 함수
-    fun solution(phone_number: String): String {
-        return "*".repeat(phone_number.length - 4) + // '*'로 가린 부분
-            phone_number.takeLast(4) // 뒷 4자리와 결합하여 반환
-    }
+    fun solution(phone_number: String): String =
+        "*".repeat(phone_number.length - 4) + phone_number.takeLast(4) // 앞부분을 *로 채우고 뒷 4자리 유지
 }
