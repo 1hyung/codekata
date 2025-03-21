1a4fc65160ae99d1abda2d57cc032780dec6f84c
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-21 11:48:44 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.08 ms, Memory: 62.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,10 +1,15 @@
 class Solution {
-    // 주어진 범위의 숫자를 순회하며 약수 개수에 따라 더하거나 뺀 결과를 반환하는 함수
-    fun solution(left: Int, right: Int): Int =
-        (left..right) // left부터 right까지의 범위 생성
-            .sumOf { n -> // 각 숫자의 결과를 계산하여 합산
-                val sqrt = Math.sqrt(n.toDouble()).toInt() // n의 제곱근을 계산하고 정수로 변환
-                if (sqrt * sqrt == n) -n // 제곱근의 제곱이 n과 같다면 제곱수 → 약수 개수는 홀수 → 음수 처리
-                else n // 그렇지 않으면 약수 개수는 짝수 → 양수 처리
+    fun solution(left: Int, right: Int): Int {
+        var answer = 0
+        for (num in left..right) {
+            // 제곱수인지 판단
+            val sqrt = Math.sqrt(num.toDouble()).toInt()
+            if (sqrt * sqrt == num) { // 제곱수이면 약수 개수 홀수
+                answer -= num 
+            } else {
+                answer += num // 제곱수가 아니면 약수 개수 짝수
             }
+        }
+        return answer
+    }
 }
