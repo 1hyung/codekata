6e55537427f6fc3f284b6eb8e66f87d51d559378
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-30 21:14:36 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -1,15 +1,8 @@
 class Solution {
     fun solution(n: Int, m: Int): IntArray {
-        // 최대공약수 구하기
-        val gcd = gcd(n, m) // 재귀 함수로 최대공약수 계산
-        // 최소공배수 구하기
-        val lcm = n * m / gcd // 최소공배수는 두 수의 곱을 최대공약수로 나눈 값
-        // 결과를 배열로 반환
-        return intArrayOf(gcd, lcm)
-    }
-
-    // 유클리드 호제법을 사용한 최대공약수 계산 함수
-    private fun gcd(a: Int, b: Int): Int {
-        return if (b == 0) a else gcd(b, a % b) // b가 0일 때 a가 최대공약수
+        fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b) // 최대공약수 재귀 함수
+        val gcdValue = gcd(n, m) // 최대공약수
+        val lcmValue = n * m / gcdValue // 최소공배수
+        return intArrayOf(gcdValue, lcmValue) // 결과 배열로 반환
     }
 }
