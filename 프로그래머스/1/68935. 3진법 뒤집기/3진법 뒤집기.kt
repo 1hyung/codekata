61f0a3db6638609326258e7942a3962065fd1812
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-31 23:19:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.51 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -1,10 +1,6 @@
 class Solution {
-    fun solution(n: Int): Int {
-        // 10진수를 3진법 문자열로 변환
-        val ternary = n.toString(radix = 3) // 예: 45 → "1200"
-        // 3진법 문자열을 뒤집음
-        val reversed = ternary.reversed() // 예: "1200" → "0021"
-        // 뒤집은 3진법을 다시 10진법 정수로 변환
-        return reversed.toInt(radix = 3) // 예: "0021" → 7
-    }
+    fun solution(n: Int): Int =
+        n.toString(3)        // 1. 3진법 문자열로 변환
+            .reversed()      // 2. 문자열을 뒤집음
+            .toInt(3)        // 3. 다시 10진법 정수로 변환
 }
