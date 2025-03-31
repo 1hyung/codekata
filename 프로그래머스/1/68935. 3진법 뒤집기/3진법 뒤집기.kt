9777bfa99798e032c66e638a033ba548473acf30
1hyung <161712242+1hyung@users.noreply.github.com>
2025-03-31 23:22:37 +0900

    [level 1] Title: 3진법 뒤집기, Time: 11.07 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(n: Int): Int =
-        n.toString(3)        // 1. 3진법 문자열로 변환
-            .reversed()      // 2. 문자열을 뒤집음
-            .toInt(3)        // 3. 다시 10진법 정수로 변환
+        n.toString(3)        // 1.3진법 문자열로 변환
+            .reversed()      // 2.문자열을 뒤집음
+            .toInt(3)        // 3.다시 10진법 정수로 변환
 }
