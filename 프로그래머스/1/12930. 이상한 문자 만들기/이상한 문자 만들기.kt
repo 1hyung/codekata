b8c5f6e2e67e8a284cb2dd87cf5291dee64d2d23
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-01 19:05:34 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 18.14 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.kt
@@ -1,12 +1,9 @@
 class Solution {
-    fun solution(s: String): String {
-        return s.split(" ".toRegex()) // 공백을 포함하여 단어 분리
-            .map { word -> // 각 단어를 변환
-                word.mapIndexed { index, char ->
-                    if (index % 2 == 0) char.uppercaseChar() // 짝수 인덱스: 대문자로 변환
-                    else char.lowercaseChar() // 홀수 인덱스: 소문자로 변환
-                }.joinToString("") // 변환된 문자들을 하나의 단어로 합침
+    fun solution(s: String): String =
+        s.split(" ") // 공백을 기준으로 단어 나누기
+            .joinToString(" ") { word -> // 다시 공백으로 연결
+                word.mapIndexed { index, c ->
+                    if (index % 2 == 0) c.uppercaseChar() else c.lowercaseChar()
+                }.joinToString("")
             }
-            .joinToString(" ") // 변환된 단어들을 공백을 유지하며 합침
-    }
 }
