4903617c3f7b9f6c6216ecfc49a31be73d1146ac
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-02 23:40:19 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.kt
+++ 프로그래머스/1/131705. 삼총사/삼총사.kt
@@ -2,9 +2,9 @@ class Solution {
     fun solution(number: IntArray): Int {
         var count = 0 // 삼총사 개수 카운트
         
-        for (i in 0 until number.size - 2) { // 첫 번째 숫자 선택
-            for (j in i + 1 until number.size - 1) { // 두 번째 숫자 선택
-                for (k in j + 1 until number.size) { // 세 번째 숫자 선택
+        for (i in 0 until number.size - 2) { // 1.첫 번째 숫자 선택
+            for (j in i + 1 until number.size - 1) { // 2.두 번째 숫자 선택
+                for (k in j + 1 until number.size) { // 3.세 번째 숫자 선택
                     if (number[i] + number[j] + number[k] == 0) { // 세 수의 합이 0인지 확인
                         count++ // 삼총사 개수 증가
                     }
