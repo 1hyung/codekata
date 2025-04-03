d11f37041cda4f414e706109c577c32b3f17619b
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-03 23:30:39 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 8.70 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -1,7 +1,14 @@
 class Solution {
     fun solution(t: String, p: String): Int {
-        val pValue = p.toLong() // 비교 기준이 되는 p 값을 숫자로 변환
-        return t.windowed(p.length) // p의 길이만큼 부분 문자열 리스트 생성
-            .count { it.toLong() <= pValue } // 변환 후 p보다 작거나 같은 개수 세기
+        val pLength = p.length
+        val pValue = p.toLong()
+        var count = 0
+
+        for (i in 0..t.length - pLength) {
+            val sub = t.substring(i, i + pLength).toLong()
+            if (sub <= pValue) count++
+        }
+
+        return count
     }
 }
