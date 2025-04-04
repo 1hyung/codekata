0dedfb4001c77920701d88ec96a89c1e6354ff9d
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-04 23:33:24 +0900

    [level 1] Title: 최소직사각형, Time: 1.74 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -1,18 +1,16 @@
 class Solution {
     fun solution(sizes: Array<IntArray>): Int {
-        var maxWidth = 0  // 가장 긴 가로 길이
-        var maxHeight = 0 // 가장 긴 세로 길이
+        var maxW = 0
+        var maxH = 0
 
-        for (size in sizes) {
-            val w = size[0] // 명함의 가로 길이
-            val h = size[1] // 명함의 세로 길이
+        for ((a, b) in sizes) {
+            val w = maxOf(a, b)
+            val h = minOf(a, b)
 
-            val (big, small) = if (w > h) w to h else h to w // 큰 값을 가로, 작은 값을 세로로 설정
-
-            maxWidth = maxOf(maxWidth, big) // 현재까지의 최대 가로 길이 업데이트
-            maxHeight = maxOf(maxHeight, small) // 현재까지의 최대 세로 길이 업데이트
+            maxW = maxOf(maxW, w)
+            maxH = maxOf(maxH, h)
         }
 
-        return maxWidth * maxHeight // 최소한의 지갑 크기 반환
+        return maxW * maxH
     }
 }
