7ced1df95acd4539c85dfb7c6b2ae6861b01d00d
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-05 23:06:42 +0900

    [level 1] Title: 시저 암호, Time: 17.60 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.kt
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.kt
@@ -1,10 +1,10 @@
 class Solution {
-    fun solution(s: String, n: Int): String = 
-        s.map { 
-            when {
-                it.isUpperCase() -> 'A' + (it - 'A' + n) % 26 // 대문자 변환
-                it.isLowerCase() -> 'a' + (it - 'a' + n) % 26 // 소문자 변환
-                else -> it // 공백은 그대로 유지
+    fun solution(s: String, n: Int): String =
+        s.map { c ->
+            when (c) {
+                in 'A'..'Z' -> ((c - 'A' + n) % 26 + 'A'.code).toChar() // 대문자 처리
+                in 'a'..'z' -> ((c - 'a' + n) % 26 + 'a'.code).toChar() // 소문자 처리
+                else -> c // 공백이나 기타 문자는 그대로 유지
             }
-        }.joinToString("") // 리스트를 문자열로 변환
+        }.joinToString("")
 }
