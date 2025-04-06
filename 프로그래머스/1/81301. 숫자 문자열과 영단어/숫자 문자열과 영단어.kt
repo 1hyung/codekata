defed7f56029aa3156247634083aaaf902925b27
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-06 10:15:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 23.97 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun solution(s: String): Int {
-        val numberMap = mapOf(
-            "zero" to "0", "one" to "1", "two" to "2", "three" to "3",
-            "four" to "4", "five" to "5", "six" to "6", "seven" to "7",
-            "eight" to "8", "nine" to "9"
+        val nums = listOf(
+            "zero", "one", "two", "three", "four",
+            "five", "six", "seven", "eight", "nine"
         )
-
-        return numberMap.entries.fold(s) { acc, (word, digit) ->
-            acc.replace(word, digit) // 영단어를 숫자로 변환
-        }.toInt() // 최종 변환된 문자열을 정수로 변환하여 반환
+        var result = s
+        for (i in nums.indices) {
+            result = result.replace(nums[i], i.toString()) // 영단어를 숫자로 치환
+        }
+        return result.toInt() // 최종적으로 숫자 변환
     }
 }
