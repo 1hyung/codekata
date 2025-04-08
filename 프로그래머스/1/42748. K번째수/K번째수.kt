42e6d39e674a285391c8ea46f0704d436cdd6f62
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-08 23:19:47 +0900

    [level 1] Title: K번째수, Time: 36.10 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.kt
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -1,7 +1,10 @@
 class Solution {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
-        return commands.map { (i, j, k) ->
-            array.slice(i - 1 until j).sorted()[k - 1] // 부분 배열 추출 → 정렬 → k번째 원소 반환
-        }.toIntArray() // 결과를 IntArray로 변환
+        val answer = mutableListOf<Int>()
+        for ((i, j, k) in commands) {
+            val sliced = array.slice(i - 1 until j).sorted()
+            answer.add(sliced[k - 1])
+        }
+        return answer.toIntArray()
     }
 }
