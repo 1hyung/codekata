f0bc268d716f1739f1e17282dc9cdbb756361e5c
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-11 10:49:08 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 24.66 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -1,8 +1,13 @@
 class Solution {
     fun solution(food: IntArray): String {
-        val left = food.mapIndexed { i, count -> "$i".repeat(count / 2) }
-                       .joinToString("") // 모든 부분을 문자열로 결합
+        val sb = StringBuilder() // 좌측 음식 나열 저장용
 
-        return left + "0" + left.reversed() // 중앙 물(0) 추가 후 대칭 문자열 완성
+        for (i in 1 until food.size) {
+            val count = food[i] / 2 // 양쪽으로 나눠 가질 수 있는 수
+            repeat(count) { sb.append(i) } // 반복해서 추가
+        }
+
+        val left = sb.toString()
+        return left + "0" + left.reversed() // 가운데 물 추가하고 대칭
     }
 }
