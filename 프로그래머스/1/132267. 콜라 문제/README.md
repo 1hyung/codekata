fa30d7c26980aa95b22ad953026c340265a31a64
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-12 17:57:37 +0900

    [level 1] Title: 콜라 문제, Time: 0.07 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/README.md
+++ 프로그래머스/1/132267. 콜라 문제/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라 문제 - 132267 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267) 
 
 ### 성능 요약
 
-메모리: 61.5 MB, 시간: 0.07 ms
+메모리: 62.9 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 03일 23:43:09
+2025년 04월 12일 17:57:35
 
 ### 문제 설명
 
--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.kt
@@ -1,14 +1,14 @@
 class Solution {
     fun solution(a: Int, b: Int, n: Int): Int {
-        var totalCokes = 0
-        var emptyBottles = n
+        var empty = n // 현재 보유 중인 빈 병
+        var answer = 0 // 받은 콜라 수
 
-        while (emptyBottles >= a) {
-            val newCokes = (emptyBottles / a) * b
-            totalCokes += newCokes
-            emptyBottles = (emptyBottles % a) + newCokes
+        while (empty >= a) {
+            val exchange = (empty / a) * b // 받을 콜라 수
+            answer += exchange // 누적
+            empty = (empty % a) + exchange // 남은 병 + 새로 받은 병
         }
 
-        return totalCokes
+        return answer
     }
 }
