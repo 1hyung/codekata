e616534e96f44356a74266cf53fba0c7f0f04013
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-21 00:19:34 +0900

    [level 1] Title: 음양 더하기, Time: 9.60 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
@@ -1,6 +1,6 @@
 class Solution {
     fun solution(absolutes: IntArray, signs: BooleanArray): Int =
-        absolutes.mapIndexed { index, value -> // index와 value를 함께 사용
-            if (signs[index]) value else -value // signs 값에 따라 양수/음수 변환
-        }.sum() // 변환된 값들을 합산
+        absolutes.mapIndexed { index, value ->  // 인덱스와 값 접근
+            if (signs[index]) value else -value // 부호에 따라 음수 처리
+        }.sum() // 전체 합산
 }
