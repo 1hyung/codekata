e6f8d842b11692ff4ddfb77d76cd08e5af143d26
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-23 20:24:01 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 12.91 ms, Memory: 64.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.kt
@@ -1,4 +1,4 @@
 class Solution {
     fun solution(phone_number: String): String =
-        "*".repeat(phone_number.length - 4) + phone_number.takeLast(4) // 앞부분을 *로 채우고 뒷 4자리 유지
+        "*".repeat(phone_number.length - 4) + phone_number.takeLast(4) // 앞은 *로 채우고, 뒤 4자리는 그대로
 }
