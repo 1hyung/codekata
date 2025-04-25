70955154bdd9fc6086088024c0ec789d558f8082
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-25 23:04:46 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/0/120820. 나이 출력/나이 출력.kt
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.kt
@@ -1,4 +1,6 @@
 // age가 주어질 때 출생 연도를 return하는 함수
 class Solution {
-    fun solution(age: Int): Int = 2022 - (age - 1) // 태어난 해에 1살이므로, age - 1을 기준으로 현재 연도에서 빼줌. 단일 표현식 함수로 표현
+    fun solution(age: Int): Int {
+       return 2022 - (age - 1) // 태어난 해에 1살이므로, age - 1을 기준으로 현재 연도에서 빼줌
+    } 
 }
