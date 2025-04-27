97bcbb5d04d63f695419214c2c5edb91590bb31a
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-27 23:14:04 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.02 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.kt
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.kt
@@ -1,5 +1,6 @@
 // 두 정수의 곱을 구하는 함수
 class Solution {
-    // 간결한 단일 표현식 함수 사용 
-    fun solution(num1: Int, num2: Int): Int = num1 * num2
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
 }
