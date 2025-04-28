f437d4672205aba8addde2f3a72243f36fce6223
1hyung <161712242+1hyung@users.noreply.github.com>
2025-04-28 23:03:00 +0900

    [level 0] Title: 몫 구하기, Time: 0.02 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.kt
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.kt
@@ -1,4 +1,6 @@
-// 두 정수를 입력받아 첫 번째 정수를 두 번째 정수로 나눈 몫을 반환
+// 두 정수로 나눈 몫을 반환
 class Solution {
-    fun solution(num1: Int, num2: Int): Int = num1 / num2 // 단일 표현식 함수로 표현
+    fun solution(num1: Int, num2: Int): Int {
+        return num1 / num2
+    }
 }
