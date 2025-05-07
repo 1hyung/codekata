6f3eaf7df8b20966eb903f3efe5f52e2cbfdbeac
1hyung <161712242+1hyung@users.noreply.github.com>
2025-05-07 23:44:02 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -1,6 +1,4 @@
-// 두 수를 비교해 같으면 1, 다르면 -1를 반환
+// 두 수가 같으면 1, 다르면 -1을 반환
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        return if (num1 == num2) 1 else -1
-    }
+    fun solution(num1: Int, num2: Int): Int = if (num1 == num2) 1 else -1
 }
