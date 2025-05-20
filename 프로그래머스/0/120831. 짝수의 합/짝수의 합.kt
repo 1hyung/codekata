0ee932d336531b3bdaad58c444d05717e60cc339
1hyung <161712242+1hyung@users.noreply.github.com>
2025-05-20 22:29:03 +0900

    [level 0] Title: 짝수의 합, Time: 11.65 ms, Memory: 62.8 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
@@ -1,4 +1,6 @@
-// n 이하의 짝수를 모두 더한 값
+// n 이하의 모든 짝수를 모두 더한 값을 반환
 class Solution {
-    fun solution(n: Int): Int = (2..n step 2).sum()
+    fun solution(n: Int): Int =
+        (2..n step 2) // 2부터 n까지 2씩 증가
+            .sum() // 해당 숫자들을 모두 더함
 }
