d93e545d86fa3b502b5059def0ccad8f91f80968
1hyung <161712242+1hyung@users.noreply.github.com>
2025-05-20 22:39:20 +0900

    [level 1] Title: 약수의 합, Time: 12.06 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.kt
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.kt
@@ -1,6 +1,7 @@
+// 약수의 합
 class Solution {
-    fun solution(n: Int): Int = 
+    fun solution(n: Int): Int =
         (1..n) // 범위 생성
-        .filter { n % it == 0 } // n을 it으로 나눌 때 0이 되는 수 필터링 (약수 필터링)
-        .sum() // 약수 합산
+            .filter { n % it == 0 } // n을 it으로 나눌 때 0이 되는 수 필터링 (약수 필터링)
+            .sum() // 약수 합산
 }
