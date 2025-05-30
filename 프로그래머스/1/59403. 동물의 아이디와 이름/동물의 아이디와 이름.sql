7c51438fa9c5e82cd70867efdd919d04b271d05d
1hyung <161712242+1hyung@users.noreply.github.com>
2025-05-30 23:43:38 +0900

    [level 1] Title: 동물의 아이디와 이름, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59403. 동물의 아이디와 이름/동물의 아이디와 이름.sql
+++ 프로그래머스/1/59403. 동물의 아이디와 이름/동물의 아이디와 이름.sql
@@ -1,8 +1,8 @@
--- 동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문
+-- 동물 보호소에 들어온 모든 동물의 아이디와 이름을 ANIMAL_ID순으로 조회하는 SQL문을 작성
 SELECT
-    ANIMAL_ID, -- 동물의 ID
+    ANIMAL_ID, -- 동물의 아이디
     NAME -- 이름
-FROM 
-    ANIMAL_INS -- 데이터를 조회할 테이블 
+FROM
+    ANIMAL_INS -- 데이터를 조회할 테이블
 ORDER BY 
-    ANIMAL_ID ASC; -- ANIMAL_ID 오름차순 정렬 (ASC 생략 가능)
+    ANIMAL_ID ASC; -- ANIMAL_ID 오름차순 정렬(ASC는 생략 가능)
