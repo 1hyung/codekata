921e420c8101cb3f6e3271eb1c59640c82880272
1hyung <161712242+1hyung@users.noreply.github.com>
2025-06-03 16:58:29 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,8 +1,9 @@
+-- 상위 n개 레코드
 -- 동물 보호소에 가장 먼저 들어온 동물의 이름을 조회하는 SQL 문
 SELECT
     NAME -- 동물의 이름
 FROM 
     ANIMAL_INS -- 데이터를 조회할 테이블
-ORDER BY
+ORDER BY 
     DATETIME ASC -- 보호 시작일 기준으로 오름차순 정렬
-LIMIT 1; -- 가장 첫 번째 조회
+LIMIT 1; -- 가정 첫 번째 조회
