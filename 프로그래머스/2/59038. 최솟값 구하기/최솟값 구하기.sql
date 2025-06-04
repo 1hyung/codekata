d0bba2a264d9856c68306b7d01de8f23d6f4f8f1
1hyung <161712242+1hyung@users.noreply.github.com>
2025-06-04 22:44:38 +0900

    [level 2] Title: 최솟값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59038. 최솟값 구하기/최솟값 구하기.sql
+++ 프로그래머스/2/59038. 최솟값 구하기/최솟값 구하기.sql
@@ -1,5 +1,6 @@
+-- 최솟값 구하기 
 -- 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문
-SELECT 
-    MIN(DATETIME) AS '시간' -- 보호 시작일의 최솟값을 조회
-FROM
-    ANIMAL_INS; -- 데이터를 조회할 테이블 
+SELECT
+    min(DATETIME) AS '시간' --  보호 시작일의 최솟값 조회
+FROM 
+    ANIMAL_INS; -- 데이터를 조회할 테이블
