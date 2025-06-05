b5382d90144803838d25f0c7760c95cc7170936b
1hyung <161712242+1hyung@users.noreply.github.com>
2025-06-05 23:44:56 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,10 +1,11 @@
--- 동물 보호소에 들어온 동물 중 젊은 동물1의 아이디와 이름을 아이디 순으로 조회하는 SQL 문
+-- 어린 동물 찾기
+-- 동물 보호소에 들어온 동물 중 젊은 동물의 아이디와 이름을 아이디 순으로 조회하는 SQL 문
 SELECT
     ANIMAL_ID, -- 동물의 아이디
     NAME -- 동물의 이름
-FROM 
-    ANIMAL_INS -- 데이터를 조회할 테이블 
-WHERE
+FROM
+    ANIMAL_INS -- 데이터를 조회할 테이블
+WHERE 
     INTAKE_CONDITION != 'Aged' -- 나이가 많은 동물들 제외
 ORDER BY
-    ANIMAL_ID; -- 아이디를 오름차순으로 정렬
+    ANIMAL_ID ASC; -- 아이디를 오름차순으로 정렬
