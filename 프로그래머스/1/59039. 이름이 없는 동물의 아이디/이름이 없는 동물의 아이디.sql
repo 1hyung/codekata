748821f37dd65d66719a8de6ce445c892290a571
1hyung <161712242+1hyung@users.noreply.github.com>
2025-06-21 17:42:32 +0900

    [level 1] Title: 이름이 없는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
+++ 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
@@ -1,10 +1,11 @@
--- 동물 보호소에 들어온 동물 중, 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문
---  ID는 오름차순 정렬
+-- 이름이 없는 동물의 아이디 
+-- 이름이 없는 채로 들어온 동물의 ID를 조회하는 SQL 문을 작성
+-- ID는 오름차순 정렬
 SELECT
-    ANIMAL_ID -- 동물 ID
-FROM
-    ANIMAL_INS -- 데이터를 조회할 테이블
+    ANIMAL_ID -- 동물의 아이디 
+FROM 
+   ANIMAL_INS -- 데이터를 조회할 테이블 
 WHERE
-    NAME IS NULL -- 이름이 NULL인 조건 필터링
+    NAME IS NULL -- 이름이 없는 동물 필터링
 ORDER BY
-    ANIMAL_ID ASC; -- ID를 기준으로 오름차순 정렬
+    ANIMAL_ID ASC; -- 동물의 아이디 오름차순 정렬
