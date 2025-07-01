d581ad965a8c227412fae941c6f0c60d43c45833
1hyung <161712242+1hyung@users.noreply.github.com>
2025-07-01 23:13:57 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.12 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
@@ -1,6 +1,12 @@
+// 정수 제곱근 판별 
+// 임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단
+// n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성
+import kotlin.math.sqrt
+
 class Solution {
     fun solution(n: Long): Long {
-        val sqrt = Math.sqrt(n.toDouble()).toLong() // n의 제곱근을 구한 후 정수 변환
-        return if (sqrt * sqrt == n) (sqrt + 1) * (sqrt + 1) else -1
+        var x = sqrt(n.toDouble()).toLong() // n의 제곱근을 구하고, 정수 부분만 사용
+        
+        return if (x * x == n) (x + 1) * (x + 1) else -1
     }
 }
