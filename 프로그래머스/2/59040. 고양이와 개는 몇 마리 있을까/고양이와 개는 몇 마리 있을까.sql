4df214568eb3a7a4f504d82b0d02ee69303f2e7d
1hyung <161712242+1hyung@users.noreply.github.com>
2025-07-13 23:50:52 +0900

    [level 2] Title: 고양이와 개는 몇 마리 있을까, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -1,14 +1,14 @@
 -- 고양이와 개는 몇 마리 있을까
--- 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문
+-- 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성
 -- 고양이를 개보다 먼저 조회
-SELECT
-    ANIMAL_TYPE, -- 동물의 종류
-    COUNT(*) AS count -- 해당 동물의 개수
-FROM 
-    ANIMAL_INS -- 데이터를 조회할 테이블
-WHERE 
-    ANIMAL_TYPE IN ('Cat', 'Dog') -- 고양이와 개만 필터링
-GROUP BY 
-    ANIMAL_TYPE -- 동물의 종류별로 그룹화
-ORDER BY
-    FIELD(ANIMAL_TYPE, 'Cat', 'Dog'); -- 고양이를 먼저, 개를 나중에 정렬
+select
+    ANIMAL_TYPE, -- 동물의 종
+    count(*) as count -- 해당 동물의 수 
+from
+    ANIMAL_INS -- 데이터를 조회할 테이블 
+where 
+    ANIMAL_TYPE in ('Cat', 'Dog') -- 고양이와 개만 필터링
+group by 
+    ANIMAL_TYPE -- 동물의 종으로 그룹화 
+order by
+    field(ANIMAL_TYPE, 'Cat', 'Dog'); -- 고양이를 먼저, 개를 나중에 정렬
