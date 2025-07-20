4712dad22be23e8ea45ae4dff8f23052243fb331
1hyung <161712242+1hyung@users.noreply.github.com>
2025-07-20 15:45:34 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.02 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.kt
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.kt
@@ -1,4 +1,7 @@
 // 두 수의 합 구하기
 class Solution {
-    fun solution(num1: Int, num2: Int) = num1 + num2
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = num1 + num2
+        return answer
+    }
 }
