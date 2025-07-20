ebb869ba0d4f68f2328941b903f163f602ff2756
1hyung <161712242+1hyung@users.noreply.github.com>
2025-07-20 15:46:57 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -1,7 +1,7 @@
 // 두 수의 나눗셈
-// num1에 num2로 나눈 값에 1000을 곱한 뒤 정수 부분을 반환
-// num1에 1000을 곱한 뒤 num2로 나누면 매개변수가 Int이기 때문에 정수 반환
-// Kotlin에서는 정수 연산이므로 소수점 이하가 자동으로 제거
 class Solution {
-    fun solution(num1: Int, num2: Int) = num1 * 1000 / num2
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = num1 * 1000 / num2
+        return answer
+    }
 }
