1d3231cdf10dd4f0ffd5f38f1e12afc621c9096a
1hyung <161712242+1hyung@users.noreply.github.com>
2025-07-24 12:15:53 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 11.50 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.kt
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.kt
@@ -1,6 +1,7 @@
 // 나머지가 1이 되는 수 찾기
 class Solution {
-    fun solution(n: Int) =
-        (2 until n) // 2부터 n-1까지 범위 생성 (n 포함 x)
-            .first { n % it == 1 }// 나머지 1이 되는 첫 번째 숫자를 찾고 반환
+    fun solution(n: Int): Int {
+        var answer: Int = (1..n).first {n % it == 1}
+        return answer
+    }
 }
