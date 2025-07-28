7083254b0a882223e0e16ea517fc8759124ae9c6
1hyung <161712242+1hyung@users.noreply.github.com>
2025-07-28 23:01:09 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,8 +1,9 @@
--- 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문
+-- 중복 제거하기 
+-- 동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성
 -- 이름이 NULL인 경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
 SELECT
-    COUNT(DISTINCT NAME) AS count -- 중복되지 않은 이름의 수
-FROM
-    ANIMAL_INS -- 데이터를 조회할 테이블
-WHERE 
-    NAME IS NOT NULL; -- 이름이 NULL이 아닌 경우
+    count(DISTINCT NAME) as count -- 중복되지 않은 이름의 수 
+FROM 
+    ANIMAL_INS -- 데이터를 조회할 테이블 
+WHERE
+    NAME IS NOT NULL; -- NAME이 NULL이 아닐 때 필터링
