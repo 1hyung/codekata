78e83f070e4848ff93ab74808a1396c5a541bb44
1hyung <161712242+1hyung@users.noreply.github.com>
2025-07-30 23:08:26 +0900

    [level 2] Title: 동물 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59406. 동물 수 구하기/동물 수 구하기.sql
+++ 프로그래머스/2/59406. 동물 수 구하기/동물 수 구하기.sql
@@ -1,5 +1,7 @@
--- 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문
+-- 동물의 수 구하기 
+-- 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성
 SELECT
-    COUNT(*) AS count -- ANIMAL_ID 중 NULL값이 있을 수 있어서 모든 행을 조회
-FROM 
-    ANIMAL_INS; -- 데이터를 조회할 테이블
+    count(*) as count -- 동물의 수 구하기 
+FROM
+    ANIMAL_INS -- 데이터를 조회할 테이블 
+    
