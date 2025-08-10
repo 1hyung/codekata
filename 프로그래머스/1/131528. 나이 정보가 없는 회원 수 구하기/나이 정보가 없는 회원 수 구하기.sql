fadf838009d6631912e2a61d463fa9bd97bfb9ba
1hyung <161712242+1hyung@users.noreply.github.com>
2025-08-10 22:51:40 +0900

    [level 1] Title: 나이 정보가 없는 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
+++ 프로그래머스/1/131528. 나이 정보가 없는 회원 수 구하기/나이 정보가 없는 회원 수 구하기.sql
@@ -1,9 +1,9 @@
 -- 나이 정보가 없는 회원 수 구하기 
 -- USER_INFO 테이블에서 나이 정보가 없는 회원이 몇 명인지 출력하는 SQL문을 작성
--- 컬럼명은 USERS
+-- 컬럼명은 USERS로 지정
 SELECT
-    COUNT (USER_ID) as 'USERS'
+    COUNT(USER_ID) AS USERS -- 회원 수 구하기
 FROM 
-    USER_INFO 
+    USER_INFO -- 데이터를 조회할 테이블
 WHERE 
-    AGE IS NULL;
+    AGE IS NULL; -- 나이 정보가 없는 회원 필터링
