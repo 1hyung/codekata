b64bc59dc51e5b3a5a58fd122fbe65b09f9a4d9a
1hyung <161712242+1hyung@users.noreply.github.com>
2025-08-11 21:51:14 +0900

    [level 1] Title: 가장 비싼 상품 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
+++ 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
@@ -1,7 +1,7 @@
 -- 가장 비싼 상품 구하기
--- 판매 중인 상품 중 가장 높은 판매가를 출력하는 SQL문
--- 컬럼명은 MAX_PRICE
+--  판매 중인 상품 중 가장 높은 판매가를 출력하는 SQL문을 작성
+-- 컬럼명은 MAX_PRICE로 지정
 SELECT
-    MAX(PRICE) AS MAX_PRICE -- 가장 비싼 판매가를 MAX_PRICE로 지정
-FROM 
+    MAX(PRICE) AS 'MAX_PRICE' -- 가장 높은 판매가 출력, 컬럼명은 MAX_PRICE로 지정
+FROM
     PRODUCT; -- 데이터를 조회할 테이블
