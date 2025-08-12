67dc4cc1d89b5b9b1de5a07d579aa9902ef1df64
1hyung <161712242+1hyung@users.noreply.github.com>
2025-08-12 23:14:16 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,11 +1,11 @@
--- NULL 처리하기 
+-- NULL 처리하기
 -- 동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성
--- 이름이 없는 동물의 이름은 "No name"으로 표시
+-- 프로그래밍을 모르는 사람들은 NULL이라는 기호를 모르기 때문에, 이름이 없는 동물의 이름은 "No name"으로 표시
 SELECT
-    ANIMAL_TYPE, -- 동물의 생물 종
-    IFNULL(NAME, 'No name') AS NAME, -- 이름이 NULL이면 "No name"으로 표시
-    SEX_UPON_INTAKE -- 성별 및 중성화 여부
-FROM
-    ANIMAL_INS -- 데이터를 조회할 테이블 
+    ANIMAL_TYPE,
+    IFNULL(NAME, 'No name') AS NAME,
+    SEX_UPON_INTAKE
+FROM    
+    ANIMAL_INS
 ORDER BY
-    ANIMAL_ID ASC; -- 아이디 순으로 조회
+    ANIMAL_ID ASC
