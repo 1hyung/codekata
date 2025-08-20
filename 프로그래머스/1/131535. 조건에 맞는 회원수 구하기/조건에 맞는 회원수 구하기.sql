c5f75a3aaea005410ba2b2d5e887e54bf95bb82b
1hyung <161712242+1hyung@users.noreply.github.com>
2025-08-20 23:22:38 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,9 +1,10 @@
--- 조건에 맞는 회원수 구하기 
--- 2021년에 가입한 회원 중 나이가 20세 이상 29세 이하인 회원이 몇 명인지 출력하는 SQL문을 작성
+-- 조건에 맞는 회원수 구하기
+-- USER_INFO 테이블에서 2021년에 가입한 회원 중 나이가 20세 이상 29세 이하인 회원이 몇 명인지 출력하는 SQL문을 작성
 SELECT
-    COUNT(*) AS USERS -- 조건에 맞는 회원 수 계산
+    COUNT(USER_ID) AS USERS
 FROM 
-    USER_INFO -- 데이터를 조회할 테이블
+    USER_INFO
 WHERE
-    YEAR(JOINED) = 2021 -- 가입일이 2021년 조건
-    AND AGE BETWEEN 20 AND 29; -- 나이가 20세 이상 29세 이하인 조건
+    YEAR(JOINED) = 2021 
+    AND
+    AGE BETWEEN 20 AND 29;
