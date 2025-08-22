936088b591dad3130450d2baba804cf5d29cc16b
1hyung <161712242+1hyung@users.noreply.github.com>
2025-08-22 20:27:45 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,16 +1,15 @@
--- 중성화된 동물 파악하기
--- 중성화된 동물은 SEX_UPON_INTAKE 컬럼에 'Neutered' 또는 'Spayed'라는 단어가 들어있음
--- 동물의 아이디와 이름, 중성화 여부를 아이디 순으로 조회하는 SQL문
+-- 중성화 여부 파악하기 
+-- 동물의 아이디와 이름, 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성
 -- 중성화가 되어있다면 'O', 아니라면 'X'라고 표시
 SELECT
-    ANIMAL_ID, -- 동물의 아이디
-    NAME, -- 이름
-    CASE 
-        WHEN SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE  '%Spayed%'
-        THEN 'O' -- 중성화된 경우
-        ELSE 'X' -- 중성화되지 않은 경우
-    END AS 중성화
-FROM
-    ANIMAL_INS -- 데이터를 조회할 테이블
+    ANIMAL_ID,
+    NAME,
+    CASE WHEN
+    SEX_UPON_INTAKE LIKE '%Neutered%'OR SEX_UPON_INTAKE LIKE '%Spayed%' 
+    THEN 'O'
+    ELSE 'X'
+    END AS '중성화'
+FROM 
+    ANIMAL_INS 
 ORDER BY
-    ANIMAL_ID; -- 아이디 순으로 오름차순 정렬
+    ANIMAL_ID ASC;
