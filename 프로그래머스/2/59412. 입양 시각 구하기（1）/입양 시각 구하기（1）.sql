7bfe2eb8270a9fa1033c59feab326848a4fa4628
1hyung <161712242+1hyung@users.noreply.github.com>
2025-08-28 23:34:16 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,13 +1,15 @@
 -- 입양 시각 구하기(1)
--- 09:00부터 19:59까지 각 시간대별 입양 건수를 조회
-SELECT 
-    HOUR(DATETIME) AS HOUR,   -- DATETIME에서 시간을 추출하여 HOUR로 표시
-    COUNT(*) AS COUNT         -- 해당 시간대의 입양 건수
-FROM 
-    ANIMAL_OUTS               -- 데이터를 조회할 테이블
-WHERE 
-    HOUR(DATETIME) BETWEEN 9 AND 19 -- 시간대가 09:00부터 19:59까지인 경우 필터링
-GROUP BY 
-    HOUR(DATETIME)            -- 시간대별로 그룹화
-ORDER BY 
-    HOUR(DATETIME) ASC;           -- 시간대 순으로 정렬
+-- 보호소에서는 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 합니다. 
+-- 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성
+-- 결과는 시간대 순으로 정렬
+SELECT
+    HOUR(DATETIME) AS HOUR,
+    COUNT(DATETIME) AS COUNT
+FROM
+    ANIMAL_OUTS
+WHERE
+    HOUR(DATETIME) BETWEEN 9 AND 19
+GROUP BY
+    HOUR(DATETIME)
+ORDER BY
+    HOUR(DATETIME)
