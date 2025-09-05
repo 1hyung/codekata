c0c80221520d02d3373bdb8108238c6c0d1ee028
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-05 22:49:46 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -2,12 +2,12 @@
 -- 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회하는 SQL문을 작성
 -- 결과는 보호 시작일 순으로 조회
 SELECT
-    AI.NAME,
-    AI.DATETIME
+    AI.NAME, -- 보호소에 있는 동물 이름
+    AI.DATETIME -- 보호시작일
 FROM
-    ANIMAL_INS AI LEFT JOIN  ANIMAL_OUTS AO ON AO.ANIMAL_ID = AI.ANIMAL_ID
-WHERE   
-    AO.ANIMAL_ID IS NULL
+    ANIMAL_INS AI LEFT JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID -- LEFT JOIN으로 조건 활용
+WHERE 
+    AO.ANIMAL_ID IS NULL -- 입양을 간 동물들이 AO니까 NULL이어야 함
 ORDER BY
-    DATETIME
+    AI.DATETIME ASC -- 보호시작일을 기준으로 오름차순
 LIMIT 3;
