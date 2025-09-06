97693685476991ac9b6505dac5fd1b249ee5f342
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-06 11:35:17 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,17 +1,14 @@
 -- 카테고리 별 도서 판매량 집계하기
--- 카테고리별 2022년 1월 도서 판매량 합산 및 카테고리 오름차순 정렬
+-- 2022년 1월의 카테고리 별 도서 판매량을 합산하고, 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력하는 SQL문을 작성
+-- 결과는 카테고리명을 기준으로 오름차순 정렬
 SELECT
-    B.CATEGORY,                          -- 도서 카테고리
-    SUM(S.SALES) AS TOTAL_SALES          -- 해당 카테고리의 총 판매량
+    B.CATEGORY, -- 카테고리명
+    SUM(SALES) AS TOTAL_SALES -- 총 판매량
 FROM
-    BOOK B                               -- 도서 정보 테이블
-JOIN
-    BOOK_SALES S                         -- 도서 판매 정보 테이블
-ON
-    B.BOOK_ID = S.BOOK_ID                -- 도서 ID로 두 테이블을 연결
-WHERE
-    S.SALES_DATE BETWEEN '2022-01-01' AND '2022-01-31' -- 2022년 1월 판매 데이터 필터링
+    BOOK B LEFT JOIN BOOK_SALES BS ON BS.BOOK_ID = B.BOOK_ID -- 테이블 조인 
+WHERE 
+    YEAR(SALES_DATE) = 2022 AND MONTH(SALES_DATE) = 1 -- 2022년 1월 필터링
 GROUP BY
-    B.CATEGORY                           -- 카테고리별 그룹화
+    B.CATEGORY -- 카테고리명으로 그룹화
 ORDER BY
-    B.CATEGORY ASC;                      -- 카테고리명을 기준으로 오름차순 정렬
+    B.CATEGORY ASC; -- 카테고리명으로 오름차순 정렬
