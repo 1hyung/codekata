254cf0db00a6bcb9d7bb70b9f86e96a3640caca0
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-07 22:22:47 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
+++ 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -1,15 +1,12 @@
--- 상품코드 별 매출액(판매가 * 판매량) 합계를 계산하고 정렬
+-- 상품 별 오프라인 매출 구하기
+-- PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성
+-- 결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬
 SELECT
-    P.PRODUCT_CODE,                         -- 상품 코드
-    SUM(P.PRICE * O.SALES_AMOUNT) AS SALES -- 매출액 (판매가 * 판매량)
-FROM
-    PRODUCT P
-JOIN
-    OFFLINE_SALE O
-ON
-    P.PRODUCT_ID = O.PRODUCT_ID             -- 상품 ID로 두 테이블을 연결
+    P.PRODUCT_CODE, -- 상품코드
+    SUM(P.PRICE * OS.SALES_AMOUNT) AS SALES -- 매출액
+FROM 
+    PRODUCT P INNER JOIN OFFLINE_SALE OS ON OS.PRODUCT_ID = P.PRODUCT_ID -- 프로덕트 테이블을 기준으로 LEFT JOIN
 GROUP BY
-    P.PRODUCT_CODE                          -- 상품 코드 별로 그룹화
+    PRODUCT_CODE -- PRODUCT_CODE 별 매출액을 구해야 하니 그룹핑
 ORDER BY
-    SALES DESC,                             -- 매출액을 기준으로 내림차순 정렬
-    P.PRODUCT_CODE ASC;                     -- 매출액이 같다면 상품코드를 기준으로 오름차순 정렬
+    SALES DESC, PRODUCT_CODE ASC; -- 결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬
