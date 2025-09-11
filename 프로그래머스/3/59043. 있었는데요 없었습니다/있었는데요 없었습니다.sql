18d634e87e4e25c973ffa7508b6679da179ea197
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-11 23:22:01 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,12 +1,12 @@
 -- 있었는데요 없었습니다
 -- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성
--- 결과는 보호 시작일이 빠른 순으로 조회
+-- 결과는 보호 시작일이 빠른 순으로 조회 
 SELECT
-    AI.ANIMAL_ID,
-    AI.NAME
-FROM
-    ANIMAL_INS AI INNER JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
-WHERE
-    AO.DATETIME < AI.DATETIME
+    AI.ANIMAL_ID, -- 동물의 아이디
+    AI.NAME -- 동물의 이름
+FROM 
+    ANIMAL_INS AI INNER JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID -- 값이 있어야 하기 때문에 INNER JOIN
+WHERE 
+    AI.DATETIME > AO.DATETIME -- 보호 시작일보다 입양일이 더 빠른 경우 필터링
 ORDER BY
-    AI.DATETIME;
+    AI.DATETIME; -- 보호 시작일이 빠른 순으로 조회
