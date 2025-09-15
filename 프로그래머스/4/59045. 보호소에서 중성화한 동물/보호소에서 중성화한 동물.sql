d0e6a32a0abd0b62343d626bf8cfa7dcc6379c08
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-15 23:51:15 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,21 +1,13 @@
 -- 보호소에서 중성화한 동물
-SELECT 
-    ANIMAL_INS.ANIMAL_ID,    -- 동물의 아이디
-    ANIMAL_INS.ANIMAL_TYPE,  -- 동물의 종류 (예: 개, 고양이 등)
-    ANIMAL_INS.NAME          -- 동물의 이름
+-- 보호소에서 중성화 수술을 거친 동물 정보를 알아보려 함
+-- 보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성
+SELECT
+    AI.ANIMAL_ID,
+    AI.ANIMAL_TYPE,
+    AI.NAME
 FROM 
-    ANIMAL_INS -- 데이터를 조회할 테이블 
-JOIN 
-    ANIMAL_OUTS 
-    ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID -- 입양된 동물 연결 (ANIMAL_ID 기준)
+    ANIMAL_INS AI INNER JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
 WHERE 
-    -- 보호소에 들어올 당시 중성화되지 않은 동물
-    ANIMAL_INS.SEX_UPON_INTAKE LIKE '%Intact%' 
-    AND 
-    (
-        -- 보호소에서 나갈 당시 중성화된 동물 (Spayed 또는 Neutered)
-        ANIMAL_OUTS.SEX_UPON_OUTCOME LIKE '%Spayed%' 
-        OR ANIMAL_OUTS.SEX_UPON_OUTCOME LIKE '%Neutered%'
-    )
-ORDER BY 
-    ANIMAL_INS.ANIMAL_ID ASC; -- 아이디를 기준으로 오름차순 정렬
+    AI.SEX_UPON_INTAKE != AO.SEX_UPON_OUTCOME
+ORDER BY
+    AI.ANIMAL_ID ASC;
