b398e7c31bcd79aca475ed9e7e2ba17073355248
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-16 23:40:44 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,12 +1,12 @@
 -- 보호소에서 중성화한 동물
 -- 보호소에 들어올 당시에는 중성화되지 않았지만, 보호소를 나갈 당시에는 중성화된 동물의 아이디와 생물 종, 이름을 조회하는 아이디 순으로 조회하는 SQL 문을 작성
 SELECT
-    AI.ANIMAL_ID,
-    AI.ANIMAL_TYPE,
-    AI.NAME
-FROM
-    ANIMAL_INS AI INNER JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
+    AI.ANIMAL_ID, -- 동물의 아이디
+    AI.ANIMAL_TYPE, -- 생물 종
+    AI.NAME -- 이름
+FROM 
+    ANIMAL_INS AI INNER JOIN ANIMAL_OUTS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID -- AI, AO 교집합
 WHERE
-    AI.SEX_UPON_INTAKE LIKE '%Intact%' AND (AO.SEX_UPON_OUTCOME LIKE '%Spayed%' OR AO.SEX_UPON_OUTCOME LIKE '%Neutered%')
+    AI.SEX_UPON_INTAKE LIKE '%Intact%' AND (AO.SEX_UPON_OUTCOME LIKE '%Spayed%' OR AO.SEX_UPON_OUTCOME LIKE '%Neutered%') -- AI Intact이고 AO가 Spayed거나 Neutered
 ORDER BY
-    AI.ANIMAL_ID ASC;
+    AI.ANIMAL_ID ASC; -- ANIMAL_ID 아이디 순으로 조회 
