e92ebfb4109c249f8d78c7f9a50abc989019e619
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-17 22:57:20 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -1,14 +1,13 @@
--- '경제' 카테고리에 속하는 도서의 정보 조회
-SELECT 
-    B.BOOK_ID,                                         -- 도서 ID
-    A.AUTHOR_NAME,                                     -- 저자명
-    DATE_FORMAT(B.PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE -- 포맷 변환된 출판일
+-- 조건에 맞는 도서와 저자 리스트 출력하기
+-- '경제' 카테고리에 속하는 도서들의 도서 ID(BOOK_ID), 저자명(AUTHOR_NAME), 출판일(PUBLISHED_DATE) 리스트를 출력하는 SQL문을 작성
+-- 결과는 출판일을 기준으로 오름차순 정렬
+SELECT
+    B.BOOK_ID, -- 도서 ID
+    A.AUTHOR_NAME, -- 저자명
+    DATE_FORMAT(B.PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE -- 출판일
 FROM 
-    BOOK B
-JOIN 
-    AUTHOR A
-    ON B.AUTHOR_ID = A.AUTHOR_ID                      -- BOOK과 AUTHOR 테이블 조인
-WHERE 
-    B.CATEGORY = '경제'                               -- '경제' 카테고리 조건 필터링
+    BOOK B INNER JOIN AUTHOR A ON B.AUTHOR_ID = A.AUTHOR_ID -- 둘 다 있어야 하니 INNER JOIN
+WHERE
+    CATEGORY = '경제' -- 경제 카테고리 
 ORDER BY 
-    B.PUBLISHED_DATE ASC;                             -- 출판일 오름차순 정렬
+    PUBLISHED_DATE ASC; -- 출판일 기준으로 오름차순 정렬
