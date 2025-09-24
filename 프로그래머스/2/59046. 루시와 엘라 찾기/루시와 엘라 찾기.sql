04c31dccdaec37c1f740db6614ad35a6809bbc32
1hyung <161712242+1hyung@users.noreply.github.com>
2025-09-24 23:16:42 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,12 +1,12 @@
 -- 루시와 엘라 찾기
--- 특정 이름을 가진 동물의 정보를 조회
-SELECT 
-    ANIMAL_ID,       -- 동물의 ID
-    NAME,            -- 이름
-    SEX_UPON_INTAKE  -- 성별 및 중성화 여부
-FROM 
-    ANIMAL_INS       -- 동물 보호소 테이블
+-- 동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM
+    ANIMAL_INS
 WHERE 
-    NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty') -- 이름 조건
-ORDER BY 
-    ANIMAL_ID ASC;   -- 동물 ID 기준 오름차순 정렬
+    NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY
+    ANIMAL_ID ASC;
