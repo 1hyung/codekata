def1431ad836b606cbba12fc9421ca3a9ab7c4e2
1hyung <161712242+1hyung@users.noreply.github.com>
2025-10-04 22:25:36 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -2,16 +2,16 @@
 -- 완료된 중고 거래의 총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문을 작성
 -- 결과는 총거래금액을 기준으로 오름차순 정렬
 SELECT
-    GB.WRITER_ID AS USER_ID, -- 회원 ID
-    GU.NICKNAME, -- 닉네임
-    SUM(GB.PRICE) AS TOTAL_SALES -- 총 거래금액
-FROM
-    USED_GOODS_BOARD GB INNER JOIN USED_GOODS_USER GU ON GB.WRITER_ID = GU.USER_ID -- 둘 다 값이 있어야 하니까 INNER JOIN
-WHERE 
-    STATUS IN ('DONE') -- 상태는 완료된
-GROUP BY    
-    GB.WRITER_ID -- 닉네임으로 그룹핑
-HAVING  
-    TOTAL_SALES >= 700000 -- 총 거래 금액 70만원 필터링
+    UGB.WRITER_ID AS USER_ID, -- 회원 ID
+    UGU.NICKNAME, -- 닉네임
+    SUM(UGB.PRICE) AS TOTAL_SALES -- 총거래금액
+FROM 
+    USED_GOODS_BOARD UGB INNER JOIN USED_GOODS_USER UGU ON UGB.WRITER_ID  = UGU.USER_ID -- 둘 다  있어야 하니 INNER JOIN
+WHERE
+    STATUS IN ('DONE') -- 완료된 중고 거래 필터링
+GROUP BY
+    UGB.WRITER_ID, UGU.NICKNAME -- 회원 ID와 닉네임 그룹핑 
+HAVING
+    SUM(UGB.PRICE) >= 700000 -- 총거래금액 70만원 이상 필터링
 ORDER BY
-    TOTAL_SALES ASC; -- 총거래금액 기준 오름차순 정렬
+    SUM(PRICE) ASC; -- 총거래금액 오름차순 정렬
