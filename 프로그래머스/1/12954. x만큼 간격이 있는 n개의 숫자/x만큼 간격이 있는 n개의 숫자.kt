4d4e271897ba3ca2acd7904cd7f0873849ff55f1
1hyung <161712242+1hyung@users.noreply.github.com>
2025-10-06 00:24:14 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 11.08 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.kt
@@ -1,5 +1,6 @@
+// x만큼 간격이 있는 n개의 숫자
 class Solution {
     fun solution(x: Int, n: Int): List<Long> {
-        return (1..n).map{it.toLong() * x}
+        return (1..n).map{it.toLong()*x}
     }
 }
