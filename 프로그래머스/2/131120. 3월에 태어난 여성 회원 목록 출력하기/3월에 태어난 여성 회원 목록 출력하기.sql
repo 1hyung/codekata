e8fa464850f811abedac15d412f09a14d07882ff
1hyung <161712242+1hyung@users.noreply.github.com>
2025-10-14 23:26:42 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,14 +1,15 @@
 -- 3월에 태어난 여성 회원 목록 출력하기
-SELECT 
-    MEMBER_ID,        -- 회원 ID
-    MEMBER_NAME,      -- 회원 이름
-    GENDER,           -- 성별
-    DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH -- 생년월일을 YYYY-MM-DD 형식으로 변환
-FROM 
+-- 생일이 3월인 여성 회원의 ID, 이름, 성별, 생년월일을 조회하는 SQL문을 작성
+-- 전화번호가 NULL인 경우는 출력대상에서 제외
+-- 결과는 회원ID를 기준으로 오름차순 정렬
+SELECT
+    MEMBER_ID, -- 회원의 ID
+    MEMBER_NAME, -- 이름
+    GENDER, -- 성별
+    DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH -- 생년월일
+FROM
     MEMBER_PROFILE -- 데이터를 조회할 테이블
-WHERE 
-    GENDER = 'W'                    -- 여성 회원 필터링
-    AND MONTH(DATE_OF_BIRTH) = 3     -- 생년월일이 3월인 회원 필터링
-    AND TLNO IS NOT NULL             -- 전화번호가 NULL이 아닌 회원만 조회
-ORDER BY 
-    MEMBER_ID ASC; -- 회원 ID를 기준으로 오름차순 정렬
+WHERE
+    MONTH(DATE_OF_BIRTH) = 03 AND TLNO IS NOT NULL AND GENDER IN ('W') -- 생일 3월, 전화번호 존재, 성별은 여성
+ORDER BY    
+    MEMBER_ID ASC; -- 결과는 MEMBER_ID 기준 오름차순 정렬
