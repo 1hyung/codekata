ada48075829cc0361f54d133ecda5eba5190cace
1hyung <161712242+1hyung@users.noreply.github.com>
2025-10-18 17:25:25 +0900

    [level 1] Title: 모든 레코드 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
+++ 프로그래머스/1/59034. 모든 레코드 조회하기/모든 레코드 조회하기.sql
@@ -1,7 +1,7 @@
--- 동물 보호소에 들어온 모든 동물의 정보를 조회
--- ANIMAL_ID 기준으로 오름차순 정렬
-SELECT * 
-FROM 
-    ANIMAL_INS
-ORDER BY 
-    ANIMAL_ID;
+-- 모든 레코드 조회하기
+-- 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성
+SELECT * -- 모든 정보 조회
+FROM
+    ANIMAL_INS -- 데이터 조회할 테이블
+ORDER BY
+    ANIMAL_ID ASC; -- 모든 동물의 정보를 ANIMAL_ID순으로 오름차순 정렬
