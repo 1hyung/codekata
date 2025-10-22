a6ecdacff6ec3b3171d349720b09e562217b594a
1hyung <161712242+1hyung@users.noreply.github.com>
2025-10-22 23:29:35 +0900

    [level 3] Title: 대여 기록이 존재하는 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
+++ 프로그래머스/3/157341. 대여 기록이 존재하는 자동차 리스트 구하기/대여 기록이 존재하는 자동차 리스트 구하기.sql
@@ -1,6 +1,6 @@
 -- 대여 기록이 존재하는 자동차 리스트 구하기
-SELECT
-    DISTINCT C.CAR_ID -- 중복 제거
+SELECT DISTINCT 
+    C.CAR_ID -- 중복 제거
 FROM 
     CAR_RENTAL_COMPANY_CAR C INNER JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY RH ON C.CAR_ID = RH.CAR_ID -- 두 테이블에 존재해야 하니까 inner join
 WHERE 
