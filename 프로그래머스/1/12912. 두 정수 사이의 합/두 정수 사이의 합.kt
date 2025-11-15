4fd4cb6c65d10b2ab4f49e895bc5394daa65a3ea
1hyung <161712242+1hyung@users.noreply.github.com>
2025-11-15 19:49:33 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -5,8 +5,8 @@ class Solution {
         val end = maxOf(a, b)   // a와 b 중 큰 값을 끝 값으로 설정
         val count = end - start + 1 // 범위 내 항의 개수 계산 (끝 - 시작 + 1)
 
-        return (start.toLong() + end.toLong()) * count / 2
         // (시작 값 + 끝 값) * 항의 개수 / 2 → 등차수열의 합 공식
         // Int 범위 초과를 방지하기 위해 Long으로 변환 후 계산
+        return (start.toLong() + end.toLong()) * count / 2
     }
 }
