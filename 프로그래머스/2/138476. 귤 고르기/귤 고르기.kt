3bd6f15f354077ff13409656d0949bd66bec6f08
1hyung <161712242+1hyung@users.noreply.github.com>
2026-03-30 23:26:09 +0900

    [level 2] Title: 귤 고르기, Time: 76.68 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -2,13 +2,13 @@
 
 class Solution {
     fun solution(k: Int, tangerine: IntArray): Int {
-        // 1. 각 크기별 귤의 개수를 카운트합니다.
+        // 1. 각 크기별 귤의 개수를 카운트합니다
         val countMap = mutableMapOf<Int, Int>()
         for (size in tangerine) {
             countMap[size] = countMap.getOrDefault(size, 0) + 1
         }
 
-        // 2. 개수를 내림차순으로 정렬합니다.
+        // 2. 개수를 내림차순으로 정렬합니다
         val sortedCounts = countMap.values.sortedDescending()
 
         // 3. 최소한의 종류로 k개 이상의 귤을 담기
